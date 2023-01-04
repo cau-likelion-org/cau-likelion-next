f19226d938e9fbacde1e5e18aac4912e5a84e69d
yunseonyeong <dbstjsdud99@gmail.com>
2023-01-04 20:20:06 +0900

    project setting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,26 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import '@styles/global.css';
+import Head from "next/head";
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { RecoilRoot } from "recoil";
+import type { AppProps } from 'next/app';
+import React from 'react';
+
+function CauLikeLionNext({ Component, pageProps }: AppProps) {
+
+  const queryClient = new QueryClient();
+
+
+  return (
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <Head>
+          <title>LikeLionCAU</title>
+        </Head>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </RecoilRoot >
+  );
 }
+
+export default CauLikeLionNext;
