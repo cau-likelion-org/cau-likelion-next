b1643d37f90c185535491eb664c7cfd5bc5bd2d8
d0422 <rlfehd2013@naver.com>
2023-01-12 17:43:39 +0900

    [CI/CD] getServerSideProps 테스트

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,40 @@
+import axios from 'axios';
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import Head from 'next/head';
 
-function Home() {
+interface Imovie {
+  id: number;
+  original_title: string;
+  poster_path: string;
+}
+
+function Home({ result }: InferGetServerSidePropsType<GetServerSideProps>) {
   return (
     <div>
       <Head>
         <title>setting-practice</title>
       </Head>
       <div>Home</div>
+      {result?.map((movie: Imovie) => (
+        <div key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+          <h4>{movie?.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
 
+export async function getServerSideProps() {
+  const { data } = await axios.get(
+    'https://api.themoviedb.org/3/movie/popular?api_key=0b509fc29bded6c0c259c6203d006b72',
+  );
+  const result = data?.results;
+  console.log(result);
+  return {
+    props: {
+      result,
+    },
+  };
+}
 export default Home;
