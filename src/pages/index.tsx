8f5a07bbb8335626848e560a536b4c1a24286011
yunseonyeong <dbstjsdud99@gmail.com>
2023-01-28 00:21:48 +0900

    [CHORE] Section Wrapper 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,28 +9,20 @@ import VideoSection from "src/components/home/video/VideoSection";
 
 function Landing() {
   return (
-    <LandingWrapper>
-      <SectionWrapper>
-        <VideoSection />
-        <More />
-        <IntroduceSection />
-        <VisionSection />
-        <TrackSection />
-        <ProjectSection />
-        <PlanSection />
-      </SectionWrapper>
-    </LandingWrapper>
+    <SectionWrapper>
+      <VideoSection />
+      <More />
+      <IntroduceSection />
+      <VisionSection />
+      <TrackSection />
+      <ProjectSection />
+      <PlanSection />
+    </SectionWrapper>
   );
 }
 
 export default Landing;
 
-const LandingWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 100%;
-`;
 
 const SectionWrapper = styled.div`
   display: flex;
