c1e5e5c9e9aeeb8d552fa2133763041a3a6c27d4
yunseonyeong <dbstjsdud99@gmail.com>
2023-01-30 00:08:32 +0900

    [FIX] 푸터 하단 고정 및 전체 레이아웃 높이 조정

--- src/components/common/footer/Footer.tsx
+++ src/components/common/footer/Footer.tsx
@@ -4,6 +4,7 @@ import FooterButton from './FooterButton';
 import KakaoImg from '@image/푸터카카오.svg';
 import MailImg from '@image/푸터메일.svg';
 import InstaImg from '@image/푸터인스타.svg';
+import { GreyScale } from '@utils/constant/color';
 
 const Footer = () => {
     const FooterButtonData = [
@@ -34,6 +35,9 @@ const Footer = () => {
                     ))
                 }
             </ButtonWrapper>
+            <CopyrightWrapper>
+                @ 2023 CAU LIKELION. All rights reserved.
+            </CopyrightWrapper>
         </Wrapper>
     );
 };
@@ -47,7 +51,6 @@ const Wrapper = styled.div`
     justify-content: center;
     gap: 40px;
     position: relative;
-    transform : translateY(-100%);
 `;
 
 const TitleText = styled.div`
@@ -57,9 +60,24 @@ const TitleText = styled.div`
     font-family: 'Inter';
     font-style: normal;
     font-weight: 900;
-    font-size: 27px;
+    font-size: 2.7rem;
     line-height: 41px;
     text-align: center;
+`;
+
+const CopyrightWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: ${GreyScale.light};
+    height: 79px;
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 2.1rem;
+    line-height: 32px;
+    text-align: center;
+    color: ${GreyScale.default};
 `;
 
 const ButtonWrapper = styled.div`
