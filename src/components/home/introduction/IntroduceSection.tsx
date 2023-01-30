ee76e6aa8fe448de3570c42224fd95f1451c9a23
d0422 <rlfehd2013@naver.com>
2023-01-30 21:18:22 +0900

    [FEAT] 화살표 누르면 아래 Wrapper로 이동하는 기능 추가

--- src/components/home/introduction/IntroduceSection.tsx
+++ src/components/home/introduction/IntroduceSection.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { MutableRefObject } from 'react';
 import styled from 'styled-components';
 import IntroLion from '@image/소개_인사하는 사자.gif';
 import Image from 'next/image';
 import PhotoCard from './component/PhotoCard';
 import archiving from '@image/활동기록보러가기.png';
 
-const IntroduceSection = () => {
+const IntroduceSection = ({
+  innerRef,
+}: {
+  innerRef: MutableRefObject<null>;
+}) => {
   return (
-    <Wrapper>
+    <Wrapper ref={innerRef}>
       <Image
         src={IntroLion}
         width={'180px'}
