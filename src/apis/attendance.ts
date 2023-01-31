5e6156a45a24908a752d97ea84a4f16815857e6f
d0422 <rlfehd2013@naver.com>
2023-01-31 23:22:06 +0900

    [REFACTOR] attendance GET api 목서버 api로 변경

--- src/apis/attendance.ts
+++ src/apis/attendance.ts
@@ -1,16 +1,11 @@
 import { AttendanceData, AttendanceListData } from '@@types/request';
 import axios from 'axios';
-export function getAttendance(): Promise<AttendanceData> {
-  return new Promise((resolve) => {
-    return setTimeout(() => {
-      const data: AttendanceData = {
-        name: '길동',
-        track: '프론트엔드',
-        isComplete: false,
-      };
-      resolve(data);
-    }, 1000);
-  });
+export function getAttendance() {
+  return axios
+    .get<AttendanceData>(
+      `https://f36b57ee-3ab4-4f63-8c82-b0d8282695a3.mock.pstmn.io/attendance`,
+    )
+    .then((res) => res.data);
 }
 export function postAttendance(password: string) {
   return axios.post(
