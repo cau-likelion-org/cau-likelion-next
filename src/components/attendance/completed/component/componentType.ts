dfd4948447131280cfd94d68a1ab7976c8e58660
d0422 <rlfehd2013@naver.com>
2023-02-03 02:46:34 +0900

    [REFACTOR] EntireTrack과 Track 분리 및 props 정리

---
+++ src/components/attendance/completed/component/componentType.ts
@@ -0,0 +1,8 @@
+import { MemberStackKor } from '@@types/request';
+
+export interface ITrackController {
+  title: MemberStackKor;
+  arrayLength: number;
+  titleColor: string;
+  color: string;
+}
