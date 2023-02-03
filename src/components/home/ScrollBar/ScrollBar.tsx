95b5dccad725e4373c6e8f3e45178ba3ed8b9ea0
d0422 <rlfehd2013@naver.com>
2023-02-03 21:33:10 +0900

    [FEAT] 스크롤바 기능 변경 및 스크롤바아이콘 추가

--- src/components/home/ScrollBar/ScrollBar.tsx
+++ src/components/home/ScrollBar/ScrollBar.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { motion, useScroll, useSpring } from 'framer-motion';
+import { motion, useScroll, useSpring, useTransform } from 'framer-motion';
 import styled from 'styled-components';
+import progressBar from '@image/프로그레스바.png';
+import progress from '@image/프로그레스바이동.png';
 
 const ScrollBar = () => {
   const { scrollYProgress } = useScroll();
@@ -9,26 +11,34 @@ const ScrollBar = () => {
     damping: 30,
     restDelta: 0.001,
   });
-
+  const x = useTransform(scaleX, [0, 1], ['-94%', '0%']);
   return (
-    <ScrollBarComponent
-      initial={{ opacity: 0 }}
-      whileInView={{ opacity: 1 }}
-      style={{
-        scaleX,
-      }}
-    ></ScrollBarComponent>
+    <Wrapper>
+      <ScrollBarComponent initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} style={{ x }}>
+        <ScrollBarCurrent />
+      </ScrollBarComponent>
+    </Wrapper>
   );
 };
-const ScrollBarComponent = styled(motion.div)`
+const Wrapper = styled.div`
   position: fixed;
   bottom: 0;
   left: 0;
   right: 0;
-  height: 5px;
-  background: black;
-  transform-origin: 0%;
+  height: 3px;
+  background: url(${progressBar.src}),
+    linear-gradient(to left, rgba(255, 255, 255, 0), rgba(255, 255, 255, 1), rgba(255, 255, 255, 0));
   z-index: 999;
 `;
-
+const ScrollBarComponent = styled(motion.div)`
+  width: 100%;
+`;
+const ScrollBarCurrent = styled(motion.div)`
+  background: url(${progress.src});
+  width: 58px;
+  height: 23px;
+  position: absolute;
+  bottom: -9.5px;
+  left: 96%;
+`;
 export default ScrollBar;
