81622eb4f17a06a2915f5b8fa0235f1fe0dd4b20
d0422 <rlfehd2013@naver.com>
2023-02-04 21:25:24 +0900

    [CHORE] 스크롤바 애니메이션 관련 주석 추가

--- src/components/home/ScrollBar/ScrollBar.tsx
+++ src/components/home/ScrollBar/ScrollBar.tsx
@@ -5,13 +5,16 @@ import progressBar from '@image/프로그레스바.png';
 import progress from '@image/프로그레스바이동.png';
 
 const ScrollBar = () => {
-  const { scrollYProgress } = useScroll();
+  const { scrollYProgress } = useScroll(); //스크롤 percent로 표기
+
   const scaleX = useSpring(scrollYProgress, {
     stiffness: 100,
     damping: 30,
     restDelta: 0.001,
-  });
-  const x = useTransform(scaleX, [0, 1], ['-94%', '0%']);
+  }); //스크롤 튕기는 애니메이션 효과
+
+  const x = useTransform(scaleX, [0, 1], ['-94%', '0%']); //전체 화면 너비 비율 -94% ~ 0%를 이동
+
   return (
     <Wrapper>
       <ScrollBarComponent initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} style={{ x }}>
@@ -29,10 +32,10 @@ const Wrapper = styled.div`
   background: url(${progressBar.src}),
     linear-gradient(to left, rgba(255, 255, 255, 0), rgba(255, 255, 255, 1), rgba(255, 255, 255, 0));
   z-index: 999;
-`;
+`; //스크롤바 이미지
 const ScrollBarComponent = styled(motion.div)`
   width: 100%;
-`;
+`; //투명 스크롤바(실제로 움직이는 부분)
 const ScrollBarCurrent = styled(motion.div)`
   background: url(${progress.src});
   width: 58px;
@@ -40,5 +43,5 @@ const ScrollBarCurrent = styled(motion.div)`
   position: absolute;
   bottom: -9.5px;
   left: 96%;
-`;
+`; // 스크롤바 아이콘
 export default ScrollBar;
