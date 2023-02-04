21e2b9a5120e7c97dd54fb50170b92015b8562f7
d0422 <rlfehd2013@naver.com>
2023-02-04 21:16:40 +0900

    [FEAT] IntroduceSection 반응형 적용

--- src/components/home/introduction/IntroduceSection.tsx
+++ src/components/home/introduction/IntroduceSection.tsx
@@ -8,7 +8,17 @@ import archiving from '@image/활동기록보러가기.png';
 const IntroduceSection = ({ innerRef }: { innerRef: MutableRefObject<null> }) => {
   return (
     <Wrapper ref={innerRef}>
-      <Image src={IntroLion} width={'180px'} height={'180px'} alt="소개하는 사자" />
+      <ImageWrapper>
+        <Image
+          src={IntroLion}
+          width={'180px'}
+          height={'180px'}
+          layout="fill"
+          objectFit="fill"
+          objectPosition="center"
+          alt="소개하는 사자"
+        />
+      </ImageWrapper>
       <TitleText>중앙대 멋사를 소개합니다!</TitleText>
       <SubText>중앙대학교 멋쟁이 사자처럼은 테크 기반의 아이디어를 실현하기 위해</SubText>
       <SubText>다양한 분야의 중앙인이 모였습니다.</SubText>
@@ -31,13 +41,21 @@ const Wrapper = styled.div`
   min-height: 100vh;
 `;
 
+const ImageWrapper = styled.div`
+  position: relative;
+  width: 180px;
+  height: 180px;
+  @media (max-width: 1376px) {
+    width: 100px;
+    height: 100px;
+  }
+`;
 const PhotoCardWrapper = styled.div`
   display: flex;
   justify-content: center;
   width: 100%;
   gap: 30px;
   margin: 50px 0;
-
   @media (max-width: 1200px) {
     flex-direction: column;
     width: 500px;
@@ -51,14 +69,18 @@ const TitleText = styled.div`
   font-family: 'Pretendard';
   font-style: normal;
   font-weight: 900;
-  font-size: 3.7rem;
+  font-size: 3rem;
   line-height: 76.51px;
   text-align: center;
-  margin: 15px 0;
+  margin: 14px 0;
+  @media (max-width: 1376px) {
+    font-size: 2.5rem;
+    margin: 10px 0;
+  }
 `;
 
 const SubText = styled(TitleText)`
-  font-size: 2.3rem;
+  font-size: 1.7rem;
   line-height: 40.91px;
   font-weight: 400;
   margin: 0;
