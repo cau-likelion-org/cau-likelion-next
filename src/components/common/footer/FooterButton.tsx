f053bc1d8e72d9e5b845a495835a95949c0d158f
d0422 <rlfehd2013@naver.com>
2023-02-04 22:20:47 +0900

    [CHORE] Footer 디자인 변경

--- src/components/common/footer/FooterButton.tsx
+++ src/components/common/footer/FooterButton.tsx
@@ -5,32 +5,32 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 interface FooterButtonProps {
-    Img: string;
-    link: string;
+  Img: string;
+  link: string;
 }
 
 const FooterButton = ({ Img, link }: FooterButtonProps) => {
-    return (
-        <Link href={link}>
-            <a target="_blank" rel="sponsored">
-                <Button>
-                    <Image src={Img} width={'20px'} height={'20px'} alt='share' />
-                    {/* <Img width={'30px'} height={'30px'} viewBox='0 0 50 50' preserveAspectRatio="xMinYMin meet" /> */}
-                </Button>
-            </a>
-        </Link>
-    );
+  return (
+    <Link href={link}>
+      <a target="_blank" rel="sponsored">
+        <Button>
+          <Image src={Img} width={'40px'} height={'40px'} alt="share" />
+          {/* <Img width={'30px'} height={'30px'} viewBox='0 0 50 50' preserveAspectRatio="xMinYMin meet" /> */}
+        </Button>
+      </a>
+    </Link>
+  );
 };
 
 export default FooterButton;
 
 const Button = styled.div`
-    background-color: ${Primary.default};
-    width: 40px;
-    height: 40px;
-    border-radius: 1.2rem;
-    display : inline-flex;
-    justify-content: center;
-    align-items: center;
+  background-color: ${Primary.default};
+  width: 74px;
+  height: 74px;
+  border-radius: 1.2rem;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
 `;
-
