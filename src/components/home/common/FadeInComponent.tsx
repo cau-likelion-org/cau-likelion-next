9b4085fc4affbdd603abcd814ee6f662f0e312c0
d0422 <rlfehd2013@naver.com>
2023-02-05 19:44:25 +0900

    [FEAT] 랜딩 페이드인 애니메이션 컴포넌트 추가

---
+++ src/components/home/common/FadeInComponent.tsx
@@ -0,0 +1,42 @@
+import { useAnimation, useInView, motion, Variants } from 'framer-motion';
+import { ReactElement, useEffect, useRef } from 'react';
+import styled from 'styled-components';
+
+const defaultFadeInAnimation: Variants = {
+  visible: {
+    opacity: 1,
+  },
+  hidden: {
+    opacity: 0,
+  },
+};
+
+const FadeInComponent = ({ children, variants }: { children: ReactElement; variants?: Variants }) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref);
+  const controls = useAnimation();
+
+  useEffect(() => {
+    if (isInView) {
+      controls.start('visible');
+    } else {
+      controls.start('hidden');
+    }
+  }, [controls, isInView]);
+
+  return (
+    <Wrapper
+      ref={ref}
+      animate={controls}
+      initial="hidden"
+      transition={{ delay: 0.2, duration: 3, type: 'spring' }}
+      variants={variants ? variants : defaultFadeInAnimation}
+    >
+      {children}
+    </Wrapper>
+  );
+};
+const Wrapper = styled(motion.div)`
+  width: 100%;
+`;
+export default FadeInComponent;
