12c5bbe2c6df92a822dc43b476418588103adda4
d0422 <rlfehd2013@naver.com>
2023-02-08 15:37:15 +0900

    [FEAT] 프로젝트 페이지 추가

--- src/pages/project/index.tsx
+++ src/pages/project/index.tsx
@@ -1,7 +1,44 @@
+import SearchSection from '@project/Header/SearchSection';
+import ProjectsSection from '@project/Projects/ProjectsSection';
+import { GreyScale } from '@utils/constant/color';
+import styled from 'styled-components';
 const ProjectList = () => {
-    return (
-        <>프로젝트 모아보기</>
-    );
+  return (
+    <Wrapper>
+      <Category>아카이빙 &gt; 프로젝트</Category>
+      <Title>중앙대 멋사에서 함께한 프로젝트를 소개해요!</Title>
+      <hr />
+      <SearchSection />
+      <ProjectsSection />
+    </Wrapper>
+  );
 };
 
 export default ProjectList;
+const Wrapper = styled.div`
+  &hr {
+    color: ${GreyScale.light};
+  }
+`;
+const Category = styled.div`
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 21px;
+  line-height: 153.02%;
+  @media (max-width: 1550px) {
+    font-size: 1.5rem;
+  }
+`;
+const Title = styled.div`
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 900;
+  font-size: 4rem;
+  line-height: 100%;
+  @media (max-width: 1550px) {
+    font-size: 2.3rem;
+  }
+  margin-top: 10px;
+  margin-bottom: 20px;
+`;
