33cecb60956b5971669af64f5477abc466851540
d0422 <rlfehd2013@naver.com>
2023-02-08 23:39:16 +0900

    [REFACTOR] projects를 archiving으로 변경하여 3가지 페이지에서 동일한 컴포넌트를 사용할 수 있도록 수정 IGeneration interface IArchivingData로 수정, dev_stack, description optional로 수정

--- src/components/project/Projects/ProjectsSection.tsx
+++ src/components/project/Projects/ProjectsSection.tsx
@@ -1,24 +1,23 @@
+import Archiving from '@archiving/Archiving';
 import React from 'react';
 import { useQuery } from 'react-query';
 import { getProjects } from 'src/apis/project';
-import { ObjectFlags } from 'typescript';
-import Projects from './Projects';
-export interface IGenerationData {
+
+export interface IArchivingData {
   title: string;
-  dev_stack: number[];
-  description: string;
-  event: string;
+  category: string;
   thumbnail: string;
+  dev_stack?: number[];
+  description?: string;
 }
-export type Res = Record<string, IGenerationData[]>;
+export type ProjectArrayType = Record<string, IArchivingData[]>;
 const ProjectsSection = () => {
-  const { data, isLoading } = useQuery<Res>(['projects'], getProjects);
-  console.log(data && Object.entries(data));
+  const { data, isLoading } = useQuery<ProjectArrayType>(['projects'], getProjects);
   if (isLoading) return <div>Loading...</div>;
   return (
     <>
-      {Object.entries(data as Res)!.map(([generation, value]) => (
-        <Projects generation={generation} generationData={value} key={generation} />
+      {Object.entries(data as ProjectArrayType)!.map(([generation, value]) => (
+        <Archiving archivingIndex={generation} archivingData={value} key={generation} />
       ))}
     </>
   );
