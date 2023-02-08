def2dbc9d935da7a7c8253fb9b093e083e2966a8
d0422 <rlfehd2013@naver.com>
2023-02-08 23:46:26 +0900

    [REFACTOR] Generation 네이밍 ArchivingIndex로 변경 및 getIndesMessage를 통해 archvingIndex에 따른 ArchivingIndex표기 처리

--- src/components/archiving/Archiving.tsx
+++ src/components/archiving/Archiving.tsx
@@ -1,12 +1,22 @@
+import { TRACK } from '@utils/constant';
 import React from 'react';
 import styled from 'styled-components';
 import { IArchivingData } from '../project/Projects/ProjectsSection';
 import Card from './Card';
-
+const getIndexMessage = (value: string) => {
+  const number = parseInt(value);
+  if (number <= 4) {
+    return TRACK[number];
+  }
+  if (number > 2000) {
+    return `${number}년`;
+  }
+  return `${number}기`;
+};
 const Archiving = ({ archivingIndex, archivingData }: { archivingIndex: string; archivingData: IArchivingData[] }) => {
   return (
     <Wrapper>
-      <Generation>{archivingIndex}기</Generation>
+      <ArchivingIndex>{getIndexMessage(archivingIndex)}</ArchivingIndex>
       <CardWrapper>
         {archivingData.map((data, index) => (
           <Card
@@ -27,7 +37,7 @@ export default Archiving;
 const Wrapper = styled.div`
   margin-bottom: 30px;
 `;
-const Generation = styled.div`
+const ArchivingIndex = styled.div`
   font-weight: 900;
   font-size: 2.3rem;
   margin-bottom: 20px;
