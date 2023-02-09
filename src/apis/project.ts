869cbd5ed07f6f139777050551848141dda92730
d0422 <rlfehd2013@naver.com>
2023-02-10 03:52:27 +0900

    [FIX] Res 타입 ProjectsArrayType으로 변경

--- src/apis/project.ts
+++ src/apis/project.ts
@@ -1,7 +1,7 @@
-import { Res } from '@project/Projects/ProjectsSection';
+import { ProjectsArrayType } from '@@types/request';
 import axios from 'axios';
 export function getProjects() {
   return axios
-    .get<Res>(`https://286eb829-af4d-43ed-b788-0e8e70ae0820.mock.pstmn.io/projects`)
+    .get<ProjectsArrayType>(`https://286eb829-af4d-43ed-b788-0e8e70ae0820.mock.pstmn.io/projects`)
     .then((res) => res.data);
 }
