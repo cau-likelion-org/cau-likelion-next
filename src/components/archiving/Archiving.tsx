bb963e69613a659ada1e8874527d11603909ae69
d0422 <rlfehd2013@naver.com>
2023-02-10 03:53:03 +0900

    [REFACTOR] CardWrapper grid로 변경

--- src/components/archiving/Archiving.tsx
+++ src/components/archiving/Archiving.tsx
@@ -49,8 +49,30 @@ const ArchivingIndex = styled.div`
 `;
 
 const CardWrapper = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 30px;
-  justify-content: center;
+  display: grid;
+  width: 100%;
+
+  @media (min-width: 766px) and (max-width: 1123px) {
+    width: 75%;
+    grid-template-columns: 1fr 1fr;
+    gap: 20px;
+  }
+
+  @media (min-width: 1124px) and (max-width: 1919px) {
+    width: 90%;
+    grid-template-columns: 1fr 1fr 1fr;
+    gap: 20px;
+  }
+  @media (min-width: 1920px) {
+    grid-template-columns: 1fr 1fr 1fr;
+    gap: 20px;
+  }
+
+  @media (min-width: 360px) and (max-width: 765px) {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    align-items: center;
+    justify-content: center;
+  }
 `;
