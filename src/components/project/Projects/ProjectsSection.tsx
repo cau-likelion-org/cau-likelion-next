c6c18a890a825d911e95569e50537fc53ba43a46
d0422 <rlfehd2013@naver.com>
2023-02-10 15:16:46 +0900

    [REFACTOR] getStaticProps에 따라 데이터 처리

--- src/components/project/Projects/ProjectsSection.tsx
+++ src/components/project/Projects/ProjectsSection.tsx
@@ -1,15 +1,16 @@
 import { ProjectsArrayType } from '@@types/request';
 import Archiving from '@archiving/Archiving';
+import Project from '@home/project/component/Project';
 import React from 'react';
 import { useQuery } from 'react-query';
 import { getProjects } from 'src/apis/project';
 
-const ProjectsSection = () => {
+const ProjectsSection = ({ staticData }: { staticData: ProjectsArrayType }) => {
   const { data, isLoading } = useQuery<ProjectsArrayType>(['projects'], getProjects);
-  if (isLoading) return <div>Loading...</div>;
+
   return (
     <>
-      {Object.entries(data as ProjectsArrayType)!.map(([generation, value]) => (
+      {Object.entries(isLoading ? staticData : (data as ProjectsArrayType))!.map(([generation, value]) => (
         <Archiving archivingIndex={generation} archivingData={value} key={generation} />
       ))}
     </>
