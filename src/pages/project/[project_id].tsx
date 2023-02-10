2fac2feb9af387c00c352ca6381b0c48adfbc616
d0422 <rlfehd2013@naver.com>
2023-02-10 16:57:37 +0900

    [FEAT] ProjectDetail에 Carousel 작성하여 테스트

--- src/pages/project/[project_id].tsx
+++ src/pages/project/[project_id].tsx
@@ -1,9 +1,18 @@
+import Carousel from '@archiving/Carousel';
+import pic1 from '@image/활동기록보러가기.png';
+import pic2 from '@image/projectPic.png';
+import pic3 from '@image/projectPic2.png';
+import { ReactElement } from 'react';
+import LayoutArchiving from '@common/layout/LayoutArchiving';
 const ProjectDetail = () => {
-    return (
-        <div>
-            프로젝트 게시물 상세
-        </div>
-    );
+  return (
+    <>
+      <Carousel images={[pic1, pic2, pic3]} />
+    </>
+  );
+};
+ProjectDetail.getLayout = function getLayout(page: ReactElement) {
+  return <LayoutArchiving>{page}</LayoutArchiving>;
 };
 
 export default ProjectDetail;
