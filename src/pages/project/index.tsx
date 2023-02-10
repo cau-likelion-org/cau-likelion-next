26337ce73a8e54fae511ebcec108f7c6c1876877
d0422 <rlfehd2013@naver.com>
2023-02-10 15:16:27 +0900

    [FEAT] getStaticProps 추가

--- src/pages/project/index.tsx
+++ src/pages/project/index.tsx
@@ -2,19 +2,31 @@ import Header from '@archiving/Header';
 import LayoutArchiving from '@common/layout/LayoutArchiving';
 import SearchSection from '@project/Header/SearchSection';
 import ProjectsSection from '@project/Projects/ProjectsSection';
+import { InferGetStaticPropsType } from 'next';
 import { ReactElement } from 'react';
+import { getProjects } from 'src/apis/project';
 
-const ProjectList = () => {
+const ProjectList = ({ projectStaticData }: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <>
       <Header pageName="프로젝트" introduce="중앙대 멋사에서 함께한 프로젝트를 소개해요!" />
       <SearchSection />
-      <ProjectsSection />
+      <ProjectsSection staticData={projectStaticData} />
     </>
   );
 };
+
 ProjectList.getLayout = function getLayout(page: ReactElement) {
   return <LayoutArchiving>{page}</LayoutArchiving>;
 };
 
+export async function getStaticProps() {
+  const projectStaticData = await getProjects();
+  return {
+    props: {
+      projectStaticData,
+    },
+    revalidate: 86400,
+  };
+}
 export default ProjectList;
