f0608129bbd331dded8217f67c2771dbbb6197af
d0422 <rlfehd2013@naver.com>
2023-02-10 15:10:17 +0900

    [REFACTOR] project API static props에 이용하기 위해 개선

--- src/apis/project.ts
+++ src/apis/project.ts
@@ -1,7 +1,6 @@
 import { ProjectsArrayType } from '@@types/request';
 import axios from 'axios';
-export function getProjects() {
-  return axios
-    .get<ProjectsArrayType>(`https://286eb829-af4d-43ed-b788-0e8e70ae0820.mock.pstmn.io/projects`)
-    .then((res) => res.data);
+export async function getProjects() {
+  const res = await axios.get<ProjectsArrayType>(`https://286eb829-af4d-43ed-b788-0e8e70ae0820.mock.pstmn.io/projects`);
+  return res.data;
 }
