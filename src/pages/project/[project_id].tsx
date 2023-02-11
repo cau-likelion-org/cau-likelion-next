c7f9f232e2a77cb47a92cccfe8467822a5b35306
d0422 <rlfehd2013@naver.com>
2023-02-11 22:33:24 +0900

    [REFACTOR] project_id 동적 라우팅 api 호출 컴포넌트 변경

--- src/pages/project/[project_id].tsx
+++ src/pages/project/[project_id].tsx
@@ -1,21 +1,36 @@
-import Carousel from '@archiving/Carousel';
-import pic1 from '@image/활동기록보러가기.png';
-import pic2 from '@image/projectPic.png';
-import pic3 from '@image/projectPic2.png';
-import { ReactElement } from 'react';
-import LayoutArchiving from '@common/layout/LayoutArchiving';
-import styled from 'styled-components';
-import DetailMainSection from '@project/detail/DetailMainSection';
 import { useRouter } from 'next/router';
+import { ReactElement } from 'react';
+import styled from 'styled-components';
+import { useQuery } from 'react-query';
+
+import DetailMainSection from '@project/detail/DetailMainSection';
+import Carousel from '@archiving/Carousel';
+import LayoutArchiving from '@common/layout/LayoutArchiving';
+
+import { getProjectDetail } from 'src/apis/projectDeatil';
+
+import { IProjectDetail } from '@@types/request';
+import { GreyScale } from '@utils/constant/color';
+
 const ProjectDetail = () => {
   const router = useRouter();
+  const { data, isLoading } = useQuery<IProjectDetail>(['projectDeatil', router.query.project_id], () =>
+    getProjectDetail(router.query.project_id as string),
+  );
+
+  if (isLoading) {
+    return <div>Loading</div>;
+  }
+
   return (
     <Wrapper>
-      <Carousel images={[pic1, pic2, pic3]} />
-      <DetailMainSection id={router.query.project_id as string} />
+      <Carousel images={data!.thumbnail} />
+      <DetailMainSection data={data!} />
+      <hr />
     </Wrapper>
   );
 };
+
 ProjectDetail.getLayout = function getLayout(page: ReactElement) {
   return <LayoutArchiving>{page}</LayoutArchiving>;
 };
@@ -24,4 +39,7 @@ export default ProjectDetail;
 const Wrapper = styled.div`
   width: 100%;
   height: 100%;
+  &hr {
+    background: ${GreyScale.light};
+  }
 `;
