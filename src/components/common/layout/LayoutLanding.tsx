ef519b445f67f6d50a7f1b8a1c4e90d8e3fa8fb9
yunseonyeong <dbstjsdud99@gmail.com>
2023-02-11 20:33:16 +0900

    [FEAT] 레이아웃 컴포넌트들에 MobileNavbar 추가

--- src/components/common/layout/LayoutLanding.tsx
+++ src/components/common/layout/LayoutLanding.tsx
@@ -24,12 +24,19 @@ const PageContainer = styled.div`
   background-color: ${BackgroundColor};
   min-height: calc(100vh - 184px);
   width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
   @media (max-width: 1440px) {
     padding: 100px 250px 100px 250px;
   }
   @media (max-width: 1280px) {
     padding: 100px 150px 100px 150px;
+  }
+
+  @media(max-width: 900px) {
+    padding: 100px 30px;
   }
   padding: 100px 360px 100px 360px;
 `;
