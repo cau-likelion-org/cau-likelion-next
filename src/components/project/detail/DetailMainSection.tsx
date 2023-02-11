664c65f4a9a61babc8ae4a1bf6e025236e6f6062
d0422 <rlfehd2013@naver.com>
2023-02-11 22:33:24 +0900

    [REFACTOR] project_id 동적 라우팅 api 호출 컴포넌트 변경

--- src/components/project/detail/DetailMainSection.tsx
+++ src/components/project/detail/DetailMainSection.tsx
@@ -1,37 +1,21 @@
-import { IProjectDetail } from '@@types/request';
-import React from 'react';
-import { useQuery } from 'react-query';
-import { getProjectDetail } from 'src/apis/projectDeatil';
 import ProjectIntroduction from './ProjectIntroductionSection';
 import TeamIntroducton from './TeamIntroductonSection';
 import styled from 'styled-components';
-import { GreyScale } from '@utils/constant/color';
+import { IProjectDetail } from '@@types/request';
 
-const DetailMainSection = ({ id }: { id: string }) => {
-  const { data, isLoading } = useQuery<IProjectDetail>(['projectDeatil', id], () => getProjectDetail(id));
-  console.log(data);
-  if (data)
-    return (
-      <Wrapper>
-        <FlexBox>
-          <TeamIntroducton projectData={data} />
-          <ProjectIntroduction projectData={data} />
-        </FlexBox>
-        <hr />
-      </Wrapper>
-    );
-  return <div></div>;
+const DetailMainSection = ({ data }: { data: IProjectDetail }) => {
+  return (
+    <Wrapper>
+      <TeamIntroducton projectData={data} />
+      <ProjectIntroduction projectData={data} />
+    </Wrapper>
+  );
 };
 
 export default DetailMainSection;
-const FlexBox = styled.div`
+const Wrapper = styled.div`
   display: flex;
   gap: 32px;
   margin: 30px 0;
-`;
-const Wrapper = styled.div`
   width: 100%;
-  &hr {
-    background: ${GreyScale.light};
-  }
 `;
