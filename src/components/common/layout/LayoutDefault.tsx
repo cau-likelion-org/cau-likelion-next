0bee189122c23a6a7ce9d63c1558b7e33c53d952
yunseonyeong <dbstjsdud99@gmail.com>
2023-02-11 20:33:16 +0900

    [FEAT] 레이아웃 컴포넌트들에 MobileNavbar 추가

--- src/components/common/layout/LayoutDefault.tsx
+++ src/components/common/layout/LayoutDefault.tsx
@@ -3,11 +3,13 @@ import NavBar from '@common/header/NavBar';
 import Footer from '@common/footer/Footer';
 import styled from 'styled-components';
 import { ReactElement } from 'react';
+import MobileNavBar from '@common/header/MobileNavBar';
 
 const LayoutDefault = ({ children }: { children: ReactElement; }) => {
   return (
     <>
       <NavBar />
+      <MobileNavBar />
       <main>
         <PageContainer>{children}</PageContainer>
       </main>
@@ -21,12 +23,19 @@ const PageContainer = styled.div`
   background-color: ${BackgroundColor};
   min-height: calc(100vh - 184px);
   width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
   @media (max-width: 1440px) {
     padding: 100px 250px 100px 250px;
   }
   @media (max-width: 1280px) {
     padding: 100px 150px 100px 150px;
+  }
+
+  @media(max-width: 900px) {
+    padding: 100px 30px;
   }
   padding: 100px 360px 100px 360px;
 `;
