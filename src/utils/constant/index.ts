323ad019d6682671725648c606e30fabb4f6ac65
yunseonyeong <dbstjsdud99@gmail.com>
2023-02-12 17:52:10 +0900

    [CHORE] 내 출석 현황, 내 프로필 인터페이스 추가

--- src/utils/constant/index.ts
+++ src/utils/constant/index.ts
@@ -13,6 +13,7 @@ export enum TRACK {
   FRONTEND,
   BACKEND,
 }
+
 export enum DEV_STACK {
   React,
   Typescript,
@@ -22,21 +23,21 @@ export enum DEV_STACK {
   Django,
   //변동 가능
 }
-export const TRACK_INDEX: { [key: string]: number } = {
+export const TRACK_INDEX: { [key: string]: number; } = {
   기획: TRACK.PM,
   디자인: TRACK.DESIGN,
   프론트엔드: TRACK.FRONTEND,
   백엔드: TRACK.BACKEND,
 };
 
-export const TRACK_NAME: { [key: number]: string } = {
+export const TRACK_NAME: { [key: number]: string; } = {
   [TRACK.PM]: '기획',
   [TRACK.DESIGN]: '디자인',
   [TRACK.FRONTEND]: '프론트엔드',
   [TRACK.BACKEND]: '백엔드',
 };
 
-export const TRACK_DESCRIPTION: { [key: number]: { description: string; recommend: string } } = {
+export const TRACK_DESCRIPTION: { [key: number]: { description: string; recommend: string; }; } = {
   [TRACK.PM]: {
     description: '기획 트랙에서는 어쩌고 저쩌고 저쩌고 저쩌고 배웁니다!',
     recommend: '평소에 기획에 관심이 많고 어쩌고 저쩌고 한 분들께 추천합니다.',
