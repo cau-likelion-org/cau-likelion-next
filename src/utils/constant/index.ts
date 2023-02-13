9641e02ad535b324a80df109aca203cd7a6f3f38
yunseonyeong <dbstjsdud99@gmail.com>
2023-02-13 11:58:52 +0900

    [FIX] 빌드 오류 해결

--- src/utils/constant/index.ts
+++ src/utils/constant/index.ts
@@ -30,7 +30,7 @@ export const ATTENDANCE_CATEGORY_NAME: { [key: number]: string; } = {
   [ATTENDANCE_CATEGORY.NOTSUBMITTED]: '과제 미제출',
   [ATTENDANCE_CATEGORY.LATESUBMITTED]: '과제 지각제출',
   [ATTENDANCE_CATEGORY.TOTALSCORE]: '총점'
-}
+};
 
 export enum ACTIVITY {
   SESSION,
@@ -87,5 +87,28 @@ export const ACTIVITY_DESCRIPTION: { [key: number]: { description: string; }; } 
   },
   [ACTIVITY.STUDY]: {
     description: '중앙대 멋쟁이사자처럼은 각 트랙의 활발한 스터디 활동을 권장합니다. 익히고 싶은 신기술부터 알고리즘, CS, 기획, 디자인까지 적극적인 스터디 활동에 참여해보세요.'
+  },
+};
+
+export const TRACK_DESCRIPTION: { [key: number]: { description: string; recommend: string; }; } = {
+  [TRACK.PM]: {
+    description: '기획 트랙에서는 어쩌고 저쩌고 저쩌고 저쩌고 배웁니다!',
+    recommend: '평소에 기획에 관심이 많고 어쩌고 저쩌고 한 분들께 추천합니다.',
+  },
+  [TRACK.DESIGN]: {
+    description:
+      '디자인 트랙에서는 웹을 디자인하기 위한 기초적인 스킬을 배울 수 있어요! 디자인 툴을 익히고 다양한 웹 디자인의 이론과 용어 등 기초를 배우고 앞으로의 협업에 도움이 될 스킬을 키우는 것을 공부합니다. 이를 통해 기획자와 협업하며 주변에서 자주 보는 웹 화면을 직접 구성하고 개발자와의 소통 과정을 거쳐 적절한 디자인을 구현할 수 있어요.',
+    recommend:
+      '생각을 자유롭게 표현하는 것을 즐기는 분에게 추천해요. 팀원들과 소통을 통해서 더 멋있는 아이디어를 만드는 분을 환영합니다!',
+  },
+  [TRACK.FRONTEND]: {
+    description:
+      '프론트엔드 트랙은 사용자와 가장 가까운 개발 트랙입니다! 웹 클라이언트 개발을 위한 기초부터 심화까지의 스킬들을 배울 수 있어요! HTML, CSS, Javascript를 학습하고 NodeJS, Webpack, Babel을 거쳐 React로 나아갑니다!',
+    recommend:
+      'UI/UX에 관심있거나 디자이너와의 협업을 통해 사용자 인터페이스를 시각적으로 구현해내는 과정에 흥미를 느끼시는 분들께 추천해요!',
+  },
+  [TRACK.BACKEND]: {
+    description: '백엔드 트랙에서는 어쩌고 저쩌고 저쩌고 저쩌고 배웁니다!',
+    recommend: '평소에 백엔드에 관심이 많고 어쩌고 저쩌고 한 분들께 추천합니다.',
   },
 };
