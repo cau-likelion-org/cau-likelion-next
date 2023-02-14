36f31d1b1e2b39dd086487f07f2d5eb61b2e8323
d0422 <rlfehd2013@naver.com>
2023-02-14 21:53:09 +0900

    [REFACTOR] 훅에서 variants를 return 하도록 변경

--- src/hooks/useSlider.ts
+++ src/hooks/useSlider.ts
@@ -1,6 +1,13 @@
+import { Variants } from 'framer-motion';
 import { useState } from 'react';
 
-const useSlider = <T>(data: T[]): [index: number, direction: number, increase: () => void, decrease: () => void] => {
+const useSlider = <T>(
+  data: T[],
+  duration?: number,
+  moveScale?: number,
+  opacity?: boolean,
+  type?: 'spring' | 'linear' | 'tween' | 'inertia',
+): [index: number, direction: number, increase: () => void, decrease: () => void, animationVariant: Variants] => {
   const [[index, direction], setIndex] = useState([0, 0]);
 
   function increase() {
@@ -9,7 +16,48 @@ const useSlider = <T>(data: T[]): [index: number, direction: number, increase: (
   function decrease() {
     setIndex((prev) => (prev[0] < 1 ? [data.length - 1, -1] : [index - 1, -1]));
   }
-  return [index, direction, increase, decrease];
+
+  function getMoveScale(direction: number) {
+    if (moveScale) {
+      if (direction > 0) return moveScale;
+      else return -moveScale;
+    }
+    if (direction > 0) return 1000;
+    return -1000;
+  }
+  // 애니메이션 효과 관련 코드 initial은 초기(등장시 초기값), visible은 initial에서 랜더링될시까지 보여줄 애니메이션, exit은 visible에서 컴포넌트가 지워질때 보여줄 애니메이션
+  const animationVariant = {
+    initial: (direction: number) => {
+      return {
+        x: getMoveScale(direction),
+        opacity: opacity && 0,
+        transition: {
+          duration: duration ? duration : 0.5,
+          type: type && type,
+        },
+      };
+    },
+    visible: {
+      x: 0,
+      opacity: opacity && 1,
+      transition: {
+        duration: duration ? duration : 0.5,
+        type: type && type,
+      },
+    },
+    exit: (direction: number) => {
+      return {
+        opacity: opacity && 0,
+        x: getMoveScale(direction),
+        transition: {
+          duration: duration ? duration : 0.5,
+          type: type && type,
+        },
+      };
+    },
+  };
+
+  return [index, direction, increase, decrease, animationVariant as Variants];
 };
 
 export default useSlider;
