7d5e161d29030427ddfea1ed86d9ee2c7395c6f9
d0422 <rlfehd2013@naver.com>
2023-02-16 01:49:29 +0900

    [FEAT] 텍스트 줄바꿈 반응형 적용

--- src/components/home/introduction/IntroduceSection.tsx
+++ src/components/home/introduction/IntroduceSection.tsx
@@ -8,7 +8,8 @@ import archiving from '@image/활동기록.png';
 import session from '@image/세션기록.png';
 import IntroLion from '@image/소개_인사하는 사자.gif';
 import FadeInComponent from '@home/common/FadeInComponent';
-
+const text =
+  '중앙대학교 멋쟁이사자처럼은 중앙대 학생들로 이루어진 IT 창업 동아리입니다. \n테크 기반의 아이디어를 실현하기위해 기획, 디자인, 개발 트랙 간의 끊임없는 소통을 추구하며\n다양한 프로젝트 활동을 통해 기술적 성장을 도모하고 협업 역량을 끌어올립니다.';
 const IntroduceSection = ({ innerRef }: { innerRef: MutableRefObject<null> }) => {
   return (
     <FadeInComponent>
@@ -25,9 +26,7 @@ const IntroduceSection = ({ innerRef }: { innerRef: MutableRefObject<null> }) =>
           />
         </ImageWrapper>
         <TitleText>중앙대학교 멋사를 소개합니다!</TitleText>
-        <SubText>중앙대학교 멋쟁이사자처럼은 중앙대 학생들로 이루어진 IT 창업 동아리입니다.</SubText>
-        <SubText>테크 기반의 아이디어를 실현하기 위해 기획, 디자인, 개발 트랙 간의 끊임없는 소통을 추구하며</SubText>
-        <SubText>다양한 프로젝트 활동을 통해 기술적 성장을 도모하고 협업 역량을 끌어올립니다.</SubText>
+        <SubText>{text}</SubText>
         <PhotoCardWrapper>
           <PhotoCard title={'정기세션 모아보기'} subtitle={'정기세션'} thumbnail={session.src} routing={'/session'} />
           <PhotoCard title={'활동기록 보러가기'} subtitle={'활동기록'} thumbnail={archiving.src} routing={'/gallery'} />
@@ -95,4 +94,10 @@ const SubText = styled(TitleText)`
   font-weight: 400;
   line-height: 3rem;
   margin: 0;
+  white-space: pre-wrap;
+  text-align: center;
+  @media (max-width: 900px) {
+    white-space: normal;
+    text-align: left;
+  }
 `;
