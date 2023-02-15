1dffa8839236314d241e2f8e6f46077193081319
d0422 <rlfehd2013@naver.com>
2023-02-15 18:44:00 +0900

    [FIX] useSlider getMoveScale함수로 인한 애니메이션 오류 제거

--- src/hooks/useSlider.ts
+++ src/hooks/useSlider.ts
@@ -9,7 +9,6 @@ const useSlider = <T>(
   type?: 'spring' | 'linear' | 'tween' | 'inertia',
 ): [index: number, direction: number, increase: () => void, decrease: () => void, animationVariant: Variants] => {
   const [[index, direction], setIndex] = useState([0, 0]);
-
   function increase() {
     setIndex((prev) => (prev[0] > data.length - 2 ? [0, +1] : [index + 1, +1]));
   }
@@ -17,19 +16,11 @@ const useSlider = <T>(
     setIndex((prev) => (prev[0] < 1 ? [data.length - 1, -1] : [index - 1, -1]));
   }
 
-  function getMoveScale(direction: number) {
-    if (moveScale) {
-      if (direction > 0) return moveScale;
-      else return -moveScale;
-    }
-    if (direction > 0) return 1000;
-    return -1000;
-  }
   // 애니메이션 효과 관련 코드 initial은 초기(등장시 초기값), visible은 initial에서 랜더링될시까지 보여줄 애니메이션, exit은 visible에서 컴포넌트가 지워질때 보여줄 애니메이션
   const animationVariant = {
     initial: (direction: number) => {
       return {
-        x: getMoveScale(direction),
+        x: moveScale ? (direction > 0 ? moveScale : -moveScale) : direction > 0 ? 1000 : -1000,
         opacity: opacity && 0,
         transition: {
           duration: duration ? duration : 0.5,
@@ -48,7 +39,7 @@ const useSlider = <T>(
     exit: (direction: number) => {
       return {
         opacity: opacity && 0,
-        x: getMoveScale(direction),
+        x: moveScale ? (direction < 0 ? moveScale : -moveScale) : direction < 0 ? 1000 : -1000,
         transition: {
           duration: duration ? duration : 0.5,
           type: type && type,
@@ -56,7 +47,6 @@ const useSlider = <T>(
       };
     },
   };
-
   return [index, direction, increase, decrease, animationVariant as Variants];
 };
 
