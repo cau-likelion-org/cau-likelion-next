bf9afb78734384dfdd7bf2087f4235fdcdd601c5
d0422 <rlfehd2013@naver.com>
2023-02-17 22:25:47 +0900

    [REFACTOR] login api 변경

--- src/apis/account.ts
+++ src/apis/account.ts
@@ -1,118 +1,116 @@
-import { LoginResponse, RequestSignUpForm, UserAttendance, UserProfile } from "@@types/request";
-import axios from "axios";
-import { url } from ".";
-
+import { LoginResponse, RequestSignUpForm, UserAttendance, UserProfile } from '@@types/request';
+import axios from 'axios';
+import { url } from '.';
 
 export const getEmailSecret = async (accessToken: string, emailValue: string) => {
-    // const response = await axios.get(
-    //     `${url}/cau_email`,
-    //     {
-    //         params: {
-    //             user_email: emailValue
-    //         },
-    //         headers: {
-    //             Authorization: `Bearer ${accessToken}`
-    //         }
-    //     }
-    // );
-    return { data: '1234' };
+  // const response = await axios.get(
+  //     `${url}/cau_email`,
+  //     {
+  //         params: {
+  //             user_email: emailValue
+  //         },
+  //         headers: {
+  //             Authorization: `Bearer ${accessToken}`
+  //         }
+  //     }
+  // );
+  return { data: '1234' };
 };
 
 export const postEmailSecret = (accessToken: string, secretValue: string) => {
-    // const response = axios.post(
-    //     `${url}/cau_email`,
-    //     {
-    //         secret: secretValue
-    //     },
-    //     {
-    //         headers: {
-    //             Authorization: `Bearer ${accessToken}`
-    //         }
-    //     }
-    // ).then((res) => '1234');
-    return '1235';
+  // const response = axios.post(
+  //     `${url}/cau_email`,
+  //     {
+  //         secret: secretValue
+  //     },
+  //     {
+  //         headers: {
+  //             Authorization: `Bearer ${accessToken}`
+  //         }
+  //     }
+  // ).then((res) => '1234');
+  return '1235';
 };
-
 
 export function postSignUpForm(form: RequestSignUpForm) {
-    return axios.post(
-        `${url}/signup`,
-        {
-            name: form.name,
-            generation: form.generation,
-            track: form.track,
-            is_admin: form.isAdmin,
-        },
-        {
-            headers: {
-                Authorization: `Bearer ${form.accessToken}`
-            }
-        },
-    );
-};
+  return axios.post(
+    `${url}/signup`,
+    {
+      name: form.name,
+      generation: form.generation,
+      track: form.track,
+      is_admin: form.isAdmin,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${form.accessToken}`,
+      },
+    },
+  );
+}
 
 export function patchUserProfile(userProfile: UserProfile, accessToken: string) {
-    return axios.patch(
-        `${url}/user`,
-        {
-            name: userProfile.name,
-            generation: userProfile.generation,
-            track: userProfile.track,
-            is_admin: userProfile.isAdmin,
-        },
-        {
-            headers: {
-                Authorization: `Bearer ${accessToken}`
-            }
-        }
-    );
+  return axios.patch(
+    `/user`,
+    {
+      name: userProfile.name,
+      generation: userProfile.generation,
+      track: userProfile.track,
+      is_admin: userProfile.isAdmin,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    },
+  );
 }
 
 export const getUserProfile = async (accessToken: string) => {
-    // const response = await axios.get<UserProfile>(
-    //     `${url}/user`,
-    //     {
-    //         headers: {
-    //             Authorization: `Bearer ${accessToken}`
-    //         }
-    //     }
-    // );
-    // return response.data;
-    return {
-        name: '윤선영',
-        generation: 11,
-        track: 2,
-        isAdmin: true
-    };
+  // const response = await axios.get<UserProfile>(
+  //     `${url}/user`,
+  //     {
+  //         headers: {
+  //             Authorization: `Bearer ${accessToken}`
+  //         }
+  //     }
+  // );
+  // return response.data;
+  return {
+    name: '윤선영',
+    generation: 11,
+    track: 2,
+    isAdmin: true,
+  };
 };
 
 export const getUserAttendance = async (username: string) => {
-    // const response = await axios.get<UserAttendance>(
-    //     `${url}/attendance`,
-    //     {
-    //         params: {
-    //             name: username
-    //         }
-    //     }
-    // );
-    // return response.data;
-    return {
-        name: '윤선영',
-        absence: 3,
-        truancy: 1,
-        tardiness: 2,
-        notSubmitted: 3,
-        lateSubmitted: 1,
-        totalScore: 1.5,
-    };
+  // const response = await axios.get<UserAttendance>(
+  //     `${url}/attendance`,
+  //     {
+  //         params: {
+  //             name: username
+  //         }
+  //     }
+  // );
+  // return response.data;
+  return {
+    name: '윤선영',
+    absence: 3,
+    truancy: 1,
+    tardiness: 2,
+    notSubmitted: 3,
+    lateSubmitted: 1,
+    totalScore: 1.5,
+  };
 };
 
-export function login(code: string | string[], accessToken: string) {
-    return axios.post<LoginResponse>(
-        `${url}/login`,
-        {
-            code: code,
-            accessToken: accessToken
-        }
-    ).then((res) => res.data);
+export function login(code: string | string[]) {
+  return axios
+    .post<LoginResponse>(`/api/accounts/google/callback/`, {
+      code: code,
+    })
+    .then((res) => {
+      return res.data;
+    });
 }
--- src/pages/google.tsx
+++ src/pages/google.tsx
@@ -7,38 +7,36 @@ import cookie from 'react-cookies';
 import { useMutation } from 'react-query';
 
 const Google = () => {
-    const router = useRouter();
-    const { code: code } = router.query;
-    const tokenState = useRecoilValue(accessToken);
+  const router = useRouter();
+  const { code: code } = router.query;
+  const tokenState = useRecoilValue(accessToken);
 
-    const loginHandler = useMutation({
-        mutationFn: ({ code, accessToken }: { code: string | string[]; accessToken: string; }) => login(code, accessToken),
-        onSuccess: (res) => {
-            if (!res.is_active) {
-                router.push({
-                    pathname: '/signup',
-                    query: { accessToken: res.accessToken }
-                }, '/signup');
-            }
-            else if (res.is_active) {
-                router.push('/');
-            }
-        },
-        onError: (res) => {
-            router.push('/login/failed');
-        }
-    });
-
-    useEffect(() => {
-        if (code) loginHandler.mutate({ code, accessToken: tokenState });
+  const loginHandler = useMutation({
+    mutationFn: ({ code }: { code: string | string[] }) => login(code),
+    onSuccess: (res) => {
+      if (!res.is_active) {
+        router.push(
+          {
+            pathname: '/signup',
+            query: { accessToken: res.accessToken },
+          },
+          '/signup',
+        );
+      } else if (res.is_active) {
         router.push('/');
-    }, [router, loginHandler, code, tokenState]);
+      }
+    },
+    onError: (res) => {
+      router.push('/login/failed', undefined, { shallow: true });
+    },
+  });
 
-    return (
-        <div>
-            로딩중
-        </div>
-    );
+  useEffect(() => {
+    if (code) loginHandler.mutate({ code });
+    router.push('/', undefined, { shallow: true });
+  }, [router, loginHandler, code, tokenState]);
+
+  return <div>로딩중</div>;
 };
 
 export default Google;
