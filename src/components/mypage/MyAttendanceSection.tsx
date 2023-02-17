776ad9b329991dc30d4af6c7edc66b79dbb29285
yunseonyeong <dbstjsdud99@gmail.com>
2023-02-17 22:57:47 +0900

    [FEAT] 총점 계산 useEffect

--- src/components/mypage/MyAttendanceSection.tsx
+++ src/components/mypage/MyAttendanceSection.tsx
@@ -1,7 +1,9 @@
 import { UserAttendance, UserAssignment } from '@@types/request';
 import { ATTENDANCE_CATEGORY, ATTENDANCE_CATEGORY_NAME } from '@utils/constant';
-import { GreyScale } from '@utils/constant/color';
-import React from 'react';
+import { BackgroundColor, GreyScale, Primary } from '@utils/constant/color';
+import { getTotalScore } from '@utils/index';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
 import styled from 'styled-components';
 
 const MyAttendanceSection = ({ userAttendance, userAssignment }: { userAttendance: UserAttendance; userAssignment: UserAssignment; }) => {
@@ -27,16 +29,44 @@ const MyAttendanceSection = ({ userAttendance, userAssignment }: { userAttendanc
             score: userAssignment.lateSubmitted
         },
     ];
+
+    const [totalScore, setTotalScore] = useState<number>(0);
+
+    useEffect(() => {
+        const score = getTotalScore({
+            'absence': userAttendance.absence,
+            'truancy': userAttendance.truancy,
+            'tardiness': userAttendance.tardiness,
+            'notSubmitted': userAssignment.notSubmitted,
+            'lateSubmitted': userAssignment.lateSubmitted
+        });
+        setTotalScore(score);
+    }, [userAssignment, userAttendance]);
+
     return (
         <Wrapper>
             <TitleText>출석현황</TitleText>
+            <CallOut>
+                <Row><span>기본 점수</span> 3점</Row>
+                <Row><span>지각(5분)</span> -0.5점</Row>
+                <Row><span>무단결석</span> -1.5점</Row>
+                <Row><span>결석</span> - 1점</Row >
+                <Row><span>과제 미제출</span> - 1점 </Row >
+                <Row><span>추후 제출 시</span> + 0.8점</Row >
+            </CallOut>
             <AttendanceBox>
-                {attendanceData.map((attendance, i) => (
-                    <AttendanceRow key={i} index={i}>
-                        <AttendanceTitle>{ATTENDANCE_CATEGORY_NAME[attendance.type]}</AttendanceTitle>
-                        <AttendanceScore>{attendance.score}</AttendanceScore>
-                    </AttendanceRow>
-                ))}
+                <>
+                    {attendanceData.map((attendance, i) => (
+                        <AttendanceRow key={i} index={i}>
+                            <AttendanceTitle>{ATTENDANCE_CATEGORY_NAME[attendance.type]}</AttendanceTitle>
+                            <AttendanceScore>{attendance.score}</AttendanceScore>
+                        </AttendanceRow>
+                    ))}
+                </>
+                <AttendanceRow index={5}>
+                    <AttendanceTitle>{ATTENDANCE_CATEGORY_NAME[ATTENDANCE_CATEGORY.TOTALSCORE]}</AttendanceTitle>
+                    <AttendanceScore>{totalScore}</AttendanceScore>
+                </AttendanceRow>
             </AttendanceBox>
         </Wrapper>
     );
@@ -86,4 +116,28 @@ const AttendanceTitle = styled.div`
 
 const AttendanceScore = styled(AttendanceTitle)`
     background-color: #FEFEFE;
+`;
+
+const CallOut = styled.div`
+    font-size: 1.3rem;
+    display: flex;
+    flex-wrap: wrap;
+    background-color: ${Primary.light};
+    padding: 2rem;
+    border-radius: 0.5rem;
+    justify-content: justify-content;
+    gap: 6px;
+    align-items: center;
+    margin: 1rem 0;
+`;
+
+const Row = styled.div`
+    display: flex;
+    & > span {
+        font-weight: 800;
+    }
+    gap: 10px;
+    background-color: ${BackgroundColor};
+    padding: 0.5rem 1rem;
+    border-radius: 10px;
 `;
