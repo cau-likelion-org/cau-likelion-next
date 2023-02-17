4ee289d01dfce81928944b025510b16da1cca44c
d0422 <rlfehd2013@naver.com>
2023-02-17 20:09:56 +0900

    [REFACTOR] API 변경 데이터타입 적용

--- src/apis/gallery.ts
+++ src/apis/gallery.ts
@@ -1,25 +1,25 @@
-import { ArchivingArrayType, IGalleryDetail } from '@@types/request';
+import { ArchivingArrayType, IGalleryData, IGalleryDetail } from '@@types/request';
 import axios from 'axios';
 import galleryBackupData from './backup/gallery.json';
 import galleryDetailBackupData from './backup/galleryDetail.json';
 
 export async function getGalleries() {
-    try {
-        const res = await axios.get<ArchivingArrayType>(
-            `https://286eb829-af4d-43ed-b788-0e8e70ae0820.mock.pstmn.io/galleries`,
-        );
-        return res.data;
-    } catch (err) {
-        return new Promise<ArchivingArrayType>((resolve) => resolve(galleryBackupData));
-    }
+  try {
+    const res = await axios.get<ArchivingArrayType<IGalleryData>>(
+      `https://286eb829-af4d-43ed-b788-0e8e70ae0820.mock.pstmn.io/galleries`,
+    );
+    return res.data;
+  } catch (err) {
+    return new Promise<ArchivingArrayType<IGalleryData>>((resolve) => resolve(galleryBackupData));
+  }
 }
 export async function getGalleryDetail(id: string) {
-    try {
-        const res = await axios.get<IGalleryDetail>(
-            `https://286eb829-af4d-43ed-b788-0e8e70ae0820.mock.pstmn.io/gallery/${id}`,
-        );
-        return res.data;
-    } catch (err) {
-        return new Promise<IGalleryDetail>((resolve) => resolve(galleryDetailBackupData as any));
-    }
+  try {
+    const res = await axios.get<IGalleryDetail>(
+      `https://286eb829-af4d-43ed-b788-0e8e70ae0820.mock.pstmn.io/gallery/${id}`,
+    );
+    return res.data;
+  } catch (err) {
+    return new Promise<IGalleryDetail>((resolve) => resolve(galleryDetailBackupData as any));
+  }
 }
