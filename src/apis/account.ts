a0b71669588e021200608e0b15f42f23f0affbf0
d0422 <rlfehd2013@naver.com>
2023-02-20 23:55:41 +0900

    [REFACTOR] login과 signup api분리

--- src/apis/account.ts
+++ src/apis/account.ts
@@ -1,53 +1,6 @@
 import { LoginResponse, RequestSignUpForm, UserAttendance, UserProfile } from '@@types/request';
-import axios from 'axios';
+import axios, { AxiosInstance } from 'axios';
 import { url } from '.';
-
-export const getEmailSecret = async (accessToken: string, emailValue: string) => {
-  // const response = await axios.get(
-  //     `${url}/cau_email`,
-  //     {
-  //         params: {
-  //             user_email: emailValue
-  //         },
-  //         headers: {
-  //             Authorization: `Bearer ${accessToken}`
-  //         }
-  //     }
-  // );
-  return { data: '1234' };
-};
-
-export const postEmailSecret = (accessToken: string, secretValue: string) => {
-  // const response = axios.post(
-  //     `${url}/cau_email`,
-  //     {
-  //         secret: secretValue
-  //     },
-  //     {
-  //         headers: {
-  //             Authorization: `Bearer ${accessToken}`
-  //         }
-  //     }
-  // ).then((res) => '1234');
-  return '1235';
-};
-
-export function postSignUpForm(form: RequestSignUpForm) {
-  return axios.post(
-    `${url}/signup`,
-    {
-      name: form.name,
-      generation: form.generation,
-      track: form.track,
-      is_admin: form.isAdmin,
-    },
-    {
-      headers: {
-        Authorization: `Bearer ${form.accessToken}`,
-      },
-    },
-  );
-}
 
 export function patchUserProfile(userProfile: UserProfile, accessToken: string) {
   return axios.patch(
@@ -116,7 +69,6 @@ export function login(code: string | string[]) {
 }
 
 export function getAcessToken(refresh_code: string) {
-  console.log('코드:', refresh_code);
   return axios
     .post(`/api/auths/token/refresh/`, {
       refresh: refresh_code,
---
+++ src/apis/signUp.ts
@@ -0,0 +1,68 @@
+import { RequestSignUpForm } from '@@types/request';
+import axios from 'axios';
+import Axios from 'axios';
+import { getAcessToken } from './account';
+const getSignUpAxiosInstance = (accessToken: string, refreshToken: string) => {
+  const signUpAxiosInstance = Axios.create({
+    headers: {
+      Authorization: `${accessToken}`,
+    },
+  });
+  signUpAxiosInstance.interceptors.response.use(
+    (res) => res,
+    async (error) => {
+      const {
+        config,
+        response: { status },
+      } = error;
+      if (status !== 401) {
+        return Promise.reject(error);
+      }
+      const { access: newAccessToken } = await getAcessToken(refreshToken);
+
+      config.headers.Authorization = `Bearer ${newAccessToken}`;
+      return axios(config);
+    },
+  );
+  return signUpAxiosInstance;
+};
+
+export const getEmailSecret = async (accessToken: string, refreshToken: string, emailValue: string) => {
+  const axiosInstance = getSignUpAxiosInstance(accessToken, refreshToken);
+  const response = await axiosInstance.get(`/api/accounts/caumail`, {
+    params: { email: emailValue },
+  });
+  return response;
+};
+
+export const postEmailSecret = (accessToken: string, secretValue: string) => {
+  // const response = axios.post(
+  //     `${url}/cau_email`,
+  //     {
+  //         secret: secretValue
+  //     },
+  //     {
+  //         headers: {
+  //             Authorization: `Bearer ${accessToken}`
+  //         }
+  //     }
+  // ).then((res) => '1234');
+  return '1235';
+};
+
+export function postSignUpForm(form: RequestSignUpForm) {
+  return axios.post(
+    `/signup`,
+    {
+      name: form.name,
+      generation: form.generation,
+      track: form.track,
+      is_admin: form.isAdmin,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${form.accessToken}`,
+      },
+    },
+  );
+}
