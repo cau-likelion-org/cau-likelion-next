5c42874f3368d1ee517cbfc765878e0c5e27864c
Yu Harin <yuharin0707@naver.com>
2023-02-21 21:57:21 +0900

    [FEAT] 세션 상세페이지 라우팅

---
+++ src/components/session/SessionDetailSection.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ISessionDetail } from '@@types/request';
+
+const SessionDetailSection = ({ sessionDetail }: { sessionDetail: ISessionDetail; }) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default SessionDetailSection;
