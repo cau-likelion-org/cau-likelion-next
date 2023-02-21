9c1454669d8ef2cb07b43104c6125b7c0df2a4c8
Yu Harin <yuharin0707@naver.com>
2023-02-21 10:53:35 +0900

    [REFACTOR] 데이터 배열 길이 에 따라 보여줄 슬라이드 수 조정

--- src/components/session/Slick.tsx
+++ src/components/session/Slick.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
+import {useState} from 'react';
 import Slider from 'react-slick';
-
-import Arrow from './Arrow';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
@@ -15,22 +14,75 @@ type  ModalProps = {
 
 
 const Slick:React.FC<ModalProps> = ({trackData, trackName}) => {
-    const settings={
-        infinite: true,
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 4,
-        arrows: true,
-        // prevArrow:(<Arrow direction='left' />),
-        // nextArrow:(<Arrow direction='right' />)
+    const [active, setActive] = useState(1);
+    const length = trackData.length;
 
+    const slidesToShowArr = setSlidesToShow();
+
+    function setSlidesToShow(){
+        let arr=[]
+        let i = 4;
+        while(i>0){
+            if(length<i){
+                arr.push(length)
+            }
+            else{
+                arr.push(i)
+            }
+            i--;
+        }
+        return[...arr];
     }
+    
+    console.log(slidesToShowArr);
+
+
+    
+
+    const settings1={
+        infinite: true,
+        speed: 1000,
+        // variableWidth: true,
+        // centerMode: true,
+
+        responsive:[
+            {breakpoint: 1920,
+            settings:{
+                slidesToShow: slidesToShowArr[0],
+                slidesToScroll:slidesToShowArr[0],
+            }},
+            {breakpoint: 1660,
+                settings:{
+                    slidesToShow: slidesToShowArr[1],
+                    slidesToScroll: slidesToShowArr[1],
+            }},
+            {breakpoint:1220,
+                settings:{
+                    slidesToShow: slidesToShowArr[2],
+                    slidesToScroll:slidesToShowArr[2],
+            }},
+            {breakpoint:300,
+                settings:{
+                    slidesToShow:slidesToShowArr[3],
+                    slidesToScroll:slidesToShowArr[3],
+            }}
+
+        ]
+    }
+
+    // @media (min-width: 1920px) {grid-template-columns: 1fr 1fr 1fr; gap: 20px;}
+
+    // @media (min-width: 1220px) and (max-width: 1660px) {grid-template-columns: 1fr 1fr; gap: 30px;}
+
+    // @media (min-width: 870px) and (max-width: 1221px) {grid-template-columns: 1fr 1fr; gap: 30px;;}
+
+    // @media (min-width: 360px) and (max-width: 871px) {grid-template-columns: 1fr; gap: 30px;}
+
 
 
     return (
         <Wrapper>
-            
-            <StSlider {...settings} className='cardWrapper'>
+            <StSlider  {...settings1} className='cardWrapper'>
                 {trackData.slice(0).reverse().map((data)=>{
                     return(
                         <Card
@@ -40,9 +92,10 @@ const Slick:React.FC<ModalProps> = ({trackData, trackName}) => {
                         thumbnail={data.thumbnail}
                         title={data.title}
                         category={`${data.category}차 세션`} />
-                        )
+                    )
                     })}
-            </StSlider>
+                    </StSlider>
+            
         </Wrapper>
         
     );
@@ -55,13 +108,14 @@ margin: 0;
 padding: 0;
 width: 70vw;
 
+
 `;
 
 
 const StSlider = styled(Slider)`
+
 .slick-prev,
 .slick-next {
-    background-color: red;
     width: 7rem;
     height: 7rem;
     cursor: pointer;
@@ -71,6 +125,7 @@ const StSlider = styled(Slider)`
     border: none;
     outline: none;
     background: transparent;
+    z-index: 1;
 
 }
 
