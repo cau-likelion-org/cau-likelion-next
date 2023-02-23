f98b2c7f016673bcc7141b52b30551c09a8b342f
d0422 <rlfehd2013@naver.com>
2023-02-23 13:33:40 +0900

    [REFACTOR] token 타입 변경

--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -1,5 +1,5 @@
 import { UserAttendance, UserProfile } from '@@types/request';
-import { accessToken } from '@utils/state';
+import { token } from '@utils/state';
 import { AxiosError } from 'axios';
 import React, { useEffect } from 'react';
 import { useQuery } from 'react-query';
@@ -12,69 +12,70 @@ import ProfileCard from '@mypage/component/ProfileCard';
 import { GreyScale } from '@utils/constant/color';
 
 const MyPage = () => {
-    const tokenState = useRecoilValue(accessToken);
+  const tokenState = useRecoilValue(token);
 
-    const { data: userProfile, isLoading: profileLoading, error: profileError } = useQuery<UserProfile, AxiosError>(
-        ['userProfile', tokenState],
-        () => getUserProfile(tokenState)
-    );
+  const {
+    data: userProfile,
+    isLoading: profileLoading,
+    error: profileError,
+  } = useQuery<UserProfile, AxiosError>(['userProfile', tokenState], () => getUserProfile(tokenState));
 
-    const { data: userAttendance, isLoading: attendanceLoading, error: attendanceError } = useQuery<UserAttendance, AxiosError>(
-        ['userAttendance', userProfile?.name],
-        () => getUserAttendance(userProfile!.name),
-        {
-            enabled: !!userProfile
-        }
-    );
-
-    return (
-        <>
-            {userProfile && userAttendance &&
-                <Wrapper>
-                    <Header>
-                        <NameCard
-                            name={userProfile.name}
-                            generation={userProfile?.generation} />
-                    </Header>
-                    <RowWrapper>
-                        <ProfileCard user={userProfile} />
-                        <MyAttendanceSection userAttendance={userAttendance} />
-                    </RowWrapper>
-                </Wrapper>
-            }
-        </>
-    );
+  const {
+    data: userAttendance,
+    isLoading: attendanceLoading,
+    error: attendanceError,
+  } = useQuery<UserAttendance, AxiosError>(
+    ['userAttendance', userProfile?.name],
+    () => getUserAttendance(userProfile!.name),
+    {
+      enabled: !!userProfile,
+    },
+  );
+  console.log(userProfile);
+  return (
+    <>
+      {userProfile && userAttendance && (
+        <Wrapper>
+          <Header>
+            <NameCard name={userProfile.name} generation={userProfile?.generation} />
+          </Header>
+          <RowWrapper>
+            <ProfileCard user={userProfile} />
+            <MyAttendanceSection userAttendance={userAttendance} />
+          </RowWrapper>
+        </Wrapper>
+      )}
+    </>
+  );
 };
 
 export default MyPage;
 
 const Wrapper = styled.div`
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 `;
 
 const Header = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    justify-content: center;
-    font-family: 'Inter';
-    font-style: normal;
-    font-weight: 400;
-    font-size: 1.6rem;
-    color: ${GreyScale.default};
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 1.6rem;
+  color: ${GreyScale.default};
 `;
 
 const RowWrapper = styled.div`
-    margin-top: 5rem;
-    display: flex;
-    justify-content: center;
-    width: 100%;
-    gap: 35px;
+  margin-top: 5rem;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  gap: 35px;
 
-    @media(max-width: 900px){
-        flex-direction: column;
-        
-    }
+  @media (max-width: 900px) {
+    flex-direction: column;
+  }
 `;
-
