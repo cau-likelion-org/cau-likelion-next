9d9dd3b70a4185cfe93afede9394cb2128061dfd
Yu Harin <yuharin0707@naver.com>
2023-02-23 18:04:56 +0900

    [CHORE] 상세 모아보기 getStaticProps 넘겨주기

--- src/apis/session.ts
+++ src/apis/session.ts
@@ -1,7 +1,16 @@
-import { SessionsArrayType } from '@@types/request';
+import { IArchivingData } from '@@types/request';
 import axios from 'axios';
+import backupData from './backup/sessionData.json';
 
 export async function getSessions() {
-    const res = await axios.get<SessionsArrayType>(`https://b3551adf-6020-42dc-b998-c947dbd93919.mock.pstmn.io/sessions`);
-    return res.data;
-}
+    try {
+        const res = await axios.get<IArchivingData>(`https://b3551adf-6020-42dc-b998-c947dbd93919.mock.pstmn.io/session`,
+        );
+
+        return res.data;
+    } catch (err) {
+        console.log(err);
+        return new Promise<IArchivingData>((resolve)=> resolve(backupData as any));
+    }
+    }
+
--- src/pages/session/index.tsx
+++ src/pages/session/index.tsx
@@ -1,30 +1,41 @@
-import {useState} from 'react';
 import Header from '@archiving/Header';
 import LayoutArchiving from '@common/layout/LayoutArchiving';
 import SessionSection from '@session/SessionSection';
 import { ReactElement } from 'react';
 import { TRACK_NAME } from '@utils/constant';
+import { getSessions } from 'src/apis/session';
+import { InferGetStaticPropsType } from 'next';
 
-import sessionData from '@session/data/sessionData.json'
+const SessionList = ({sessionStaticData}:InferGetStaticPropsType<typeof getStaticProps>) => {
+    const pm = sessionStaticData[0];
+    const design = sessionStaticData[1];
+    const front = sessionStaticData[2];
+    const back = sessionStaticData[3]
 
-const SessionList = () => {
     return (
         <>
         <Header pageName="세션" introduce="중앙대 멋사에서 진행한 세션을 소개합니다!" />
 
-        <SessionSection trackName={TRACK_NAME[0]} trackNum={0} trackData={sessionData[0]} />
-        <SessionSection trackName={TRACK_NAME[1]} trackNum={1} trackData={sessionData[1]} />
-        <SessionSection trackName={TRACK_NAME[2]} trackNum={2} trackData={sessionData[2]} />
-        <SessionSection trackName={TRACK_NAME[3]} trackNum={3} trackData={sessionData[3]} />
-
+        {[pm, design, front, back].map((data,i)=>{
+            return(
+            <SessionSection key={i} trackName={TRACK_NAME[i]} trackData={data}/>
+            )
+        })}
         </>
-);
-
-
-};
+);};
 
 SessionList.getLayout = function getLayout(page: ReactElement) {
     return <LayoutArchiving>{page}</LayoutArchiving>;
 };
+
+export async function getStaticProps({ params } : { params: { track: string;};}){
+    const sessionStaticData = await getSessions();
+    return{
+        props:{
+            sessionStaticData,
+        },
+        revalidate: 86400,
+    }
+}
 
 export default SessionList;
