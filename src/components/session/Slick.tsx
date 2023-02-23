67079125b39a2ad1633d2da57d404a67b2513899
Yu Harin <yuharin0707@naver.com>
2023-02-23 16:21:32 +0900

    [CHORE] 슬라이더 내부 카드 간격 조정

--- src/components/session/Slick.tsx
+++ src/components/session/Slick.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import {useState} from 'react';
+import styled from 'styled-components';
+import Card from '@archiving/Card';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-
-import styled from 'styled-components';
-import Card from '@archiving/Card';
 
 type  ModalProps = {
     trackName: string;
@@ -72,6 +70,7 @@ const Slick:React.FC<ModalProps> = ({trackData, trackName}) => {
 
     return (
         <Wrapper>
+            
             <StSlider  {...settings}>
                 {trackData.slice(0).reverse().map((data)=>{
                     return(
@@ -84,7 +83,8 @@ const Slick:React.FC<ModalProps> = ({trackData, trackName}) => {
                         category={`${data.category}차 세션`} />
                     )
                     })}
-                    </StSlider>
+            </StSlider>
+            
             
         </Wrapper>
         
@@ -99,7 +99,6 @@ padding: 0;
 width: 70vw;
 `;
 
-
 const StSlider = styled(Slider)`
 height: 45rem;
 
@@ -107,19 +106,18 @@ height: 45rem;
 .slick-next {
     width: 7rem;
     height: 7rem;
-    cursor: pointer;
     padding: 0;
     display: block;
     z-index: 1;
+    opacity: 0%;
+    cursor: pointer;
 
 }
 
-.slick-prev{
-    margin-left: -3rem;
-}
+.slick-prev{ margin-left: -3rem;}
 
-.slick-next{
-    margin-right:-3rem;
-}
+.slick-next{ margin-right: -3rem;}
+
+.slick-slide { padding: 40px 30px;}
+
 `
-
