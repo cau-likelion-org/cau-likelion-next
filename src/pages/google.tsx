d23e099c2f72ec8b46e2a31eb3a2b056595e7bcb
d0422 <rlfehd2013@naver.com>
2023-02-23 13:01:50 +0900

    [FEAT] 토큰에 따른 분기 처리

--- src/pages/google.tsx
+++ src/pages/google.tsx
@@ -1,14 +1,20 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { login } from 'src/apis/account';
-import { useRecoilValue } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import cookie from 'react-cookies';
 import { useMutation } from 'react-query';
+import { token } from '@utils/state';
 
 const Google = () => {
   const router = useRouter();
   const { code: code } = router.query;
-
+  const [{ access, refresh }, setToken] = useRecoilState(token);
+  useEffect(() => {
+    if (access) {
+      router.push('/');
+    }
+  }, []);
   const loginHandler = useMutation({
     mutationFn: ({ code }: { code: string | string[] }) => login(code),
     onSuccess: (res) => {
@@ -22,6 +28,13 @@ const Google = () => {
           '/signup',
         );
       } else if (res.is_active) {
+        setToken((prev) => {
+          const obj = { ...prev };
+          obj.access = res.token.access;
+          obj.refresh = res.token.refresh;
+          return obj;
+        });
+        console.log(access);
         router.push('/');
       }
     },
