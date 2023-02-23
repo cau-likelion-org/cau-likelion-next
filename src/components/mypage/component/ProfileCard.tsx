9f91d92fe53c247d6911d88a8a8f6ee031538c68
d0422 <rlfehd2013@naver.com>
2023-02-23 13:48:40 +0900

    [CHORE] isAdmin->is_admin 적용

--- src/components/mypage/component/ProfileCard.tsx
+++ src/components/mypage/component/ProfileCard.tsx
@@ -5,99 +5,100 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import UserEditModal from './UserEditModal';
 
-const ProfileCard = ({ user }: { user: UserProfile; }) => {
-    const [isEditModalOn, setIsEditModalOn] = useState(false);
+const ProfileCard = ({ user }: { user: UserProfile }) => {
+  const [isEditModalOn, setIsEditModalOn] = useState(false);
 
-    const handleUserEditModal = () => {
-        setIsEditModalOn(!isEditModalOn);
-    };
+  const handleUserEditModal = () => {
+    setIsEditModalOn(!isEditModalOn);
+  };
 
-    return (
-        <>
-            {user &&
-                <Wrapper>
-                    <NameRow>
-                        <BlueText>{user.name}</BlueText>
-                        <UserEditButton onClick={handleUserEditModal}>개인정보 변경</UserEditButton>
-                    </NameRow>
-                    <DescriptionWrapper>
-                        <DescriptionRow>
-                            <BlueText>{user.generation}기</BlueText>
-                            <Text>{user.isAdmin ? '운영진' : '아기사자'}</Text>
-                        </DescriptionRow>
-                        <DescriptionRow>
-                            <BlueText>트랙</BlueText>
-                            <Text>{TRACK_NAME[user.track]}</Text>
-                        </DescriptionRow>
-                    </DescriptionWrapper>
-                </Wrapper>
-            }
-            {isEditModalOn ? <UserEditModal userProfile={user} isEditModalOn={isEditModalOn} handleUserEditModal={handleUserEditModal} /> : null}
-        </>
-    );
+  return (
+    <>
+      {user && (
+        <Wrapper>
+          <NameRow>
+            <BlueText>{user.name}</BlueText>
+            <UserEditButton onClick={handleUserEditModal}>개인정보 변경</UserEditButton>
+          </NameRow>
+          <DescriptionWrapper>
+            <DescriptionRow>
+              <BlueText>{user.generation}기</BlueText>
+              <Text>{user.is_admin ? '운영진' : '아기사자'}</Text>
+            </DescriptionRow>
+            <DescriptionRow>
+              <BlueText>트랙</BlueText>
+              <Text>{TRACK_NAME[user.track]}</Text>
+            </DescriptionRow>
+          </DescriptionWrapper>
+        </Wrapper>
+      )}
+      {isEditModalOn ? (
+        <UserEditModal userProfile={user} isEditModalOn={isEditModalOn} handleUserEditModal={handleUserEditModal} />
+      ) : null}
+    </>
+  );
 };
 
 export default ProfileCard;
 
 const Wrapper = styled.div`
-    width: 180px;
-    height: 140px;
-    padding: 2rem;
-    background-color: ${Primary.light};
-    border-radius: 10px;
-    display: flex;
-    flex-direction: column;
+  width: 180px;
+  height: 140px;
+  padding: 2rem;
+  background-color: ${Primary.light};
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
 
-    @media(max-width: 900px){
-        width: 90%;
-        height: 200px;
-    }
+  @media (max-width: 900px) {
+    width: 90%;
+    height: 200px;
+  }
 `;
 
 const NameRow = styled.div`
-    display: flex;
-    border-bottom: 1px solid #E2E4FF;
-    width: 100%;
-    justify-content: space-between;
-    padding: 1rem 0;
+  display: flex;
+  border-bottom: 1px solid #e2e4ff;
+  width: 100%;
+  justify-content: space-between;
+  padding: 1rem 0;
 `;
 
 const DescriptionRow = styled.div`
-    display: flex;
-    gap: 8px;
+  display: flex;
+  gap: 8px;
 `;
 
 const Text = styled.div`
-    font-family: 'Pretendard';
-    font-style: normal;
-    font-weight: 500;
-    font-size: 1.5rem;
-    color: ${Basic.default};
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 1.5rem;
+  color: ${Basic.default};
 `;
 const BlueText = styled(Text)`
-    font-weight: 700;
-    color: ${Primary.default};
+  font-weight: 700;
+  color: ${Primary.default};
 `;
 
 const DescriptionWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    padding: 1rem 0;
-    gap: 8px;
+  display: flex;
+  flex-direction: column;
+  padding: 1rem 0;
+  gap: 8px;
 `;
 
 const UserEditButton = styled.div`
-    font-family: 'Pretendard';
-    font-style: normal;
-    font-weight: 500;
-    font-size: 1rem;
-    color: ${GreyScale.default};
-    background-color: #FEFEFE;
-    border-radius: 50px;
-    padding: 2px 8px;
-    cursor: pointer;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 1rem;
+  color: ${GreyScale.default};
+  background-color: #fefefe;
+  border-radius: 50px;
+  padding: 2px 8px;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
-
