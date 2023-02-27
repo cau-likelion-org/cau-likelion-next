6e58f8656509d26878681a41a68c5b311444bb5d
yunseonyeong <dbstjsdud99@gmail.com>
2023-02-28 05:50:56 +0900

    [FEAT] 출석 점수와 과제 점수를 합쳐주는 함수 추가

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,10 +1,10 @@
 // 각종 utils,추후 추가 및 수정
 
-import { AttendanceTotalScore } from "@@types/request";
+import { TotalScoreParams, UserScore } from "@@types/request";
+import { TRACK_INDEX } from "./constant";
 
 export const toDateString = (date?: Date, formatter = "-") => {
     if (!date) return "";
-
     const year = date.getFullYear();
     const month = ("0" + (1 + date.getMonth())).slice(-2);
     const day = ("0" + date.getDate()).slice(-2);
@@ -16,8 +16,33 @@ export const isEmptyString = (str: string) => {
     else return false;
 };
 
-export const getTotalScore = (data: AttendanceTotalScore) => {
+export const getTotalScore = (data: TotalScoreParams) => {
     const defaultScore = 3;
-    const totalScore = defaultScore - (1 * data.absence) + (0.8 * data.lateSubmitted) - (1 * data.notSubmitted) - (0.5 * data.tardiness) - (1.5 * data.truancy);
+    const totalScore = defaultScore - (1 * data.absence + 0.2 * data.lateSubmitted + 1 * data.notSubmitted + 0.5 * data.tardiness + 1.5 * data.truancy);
     return totalScore;
+};
+
+export const checkGeneration = (generation: number) => {
+    let year = new Date().getFullYear();
+    if (year - generation == 2012) return true;
+    return false;
+};
+
+
+
+export const getTotalNameObject = (data: any): { [name: string]: UserScore; } => {
+    let totalNameArray: { [name: string]: UserScore; } = {};
+    data.forEach((user: any, i: number) => {
+        totalNameArray[user['이름']] = {
+            name: user['이름'],
+            track: TRACK_INDEX[user['트랙']],
+            lateSubmitted: user['과제 지각제출'],
+            notSubmitted: user['과제 미제출'],
+            absence: 0,
+            truancy: 0,
+            tardiness: 0,
+            totalScore: 0,
+        };
+    });
+    return totalNameArray;
 };
