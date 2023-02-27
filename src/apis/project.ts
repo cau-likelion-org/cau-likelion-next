1f1094a794b5c884896c598436d2276d138dd7c6
d0422 <rlfehd2013@naver.com>
2023-02-27 16:57:41 +0900

    [REFACTOR] 프로젝트 api 주소 변경

--- src/apis/project.ts
+++ src/apis/project.ts
@@ -3,9 +3,7 @@ import axios from 'axios';
 import projectBackupData from './backup/project.json';
 export async function getProjects() {
   try {
-    const res = await axios.get<ArchivingArrayType<IProjectData>>(
-      `https://286eb829-af4d-43ed-b788-0e8e70ae0820.mock.pstmn.io/projects`,
-    );
+    const res = await axios.get<ArchivingArrayType<IProjectData>>(`/projects/project/`);
     return res.data;
   } catch (err) {
     console.log(err);
