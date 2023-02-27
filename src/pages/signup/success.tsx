e5896b2e20c9a5f44a2967126a31c41c98baab9e
d0422 <rlfehd2013@naver.com>
2023-02-27 15:09:55 +0900

    [FEAT] signup 페이지 구성

--- src/pages/signup/success.tsx
+++ src/pages/signup/success.tsx
@@ -1,47 +1,46 @@
 import React from 'react';
-import styled from 'styled-components'
-
+import styled from 'styled-components';
+import sign from '@image/signup.gif';
+import Image from 'next/image';
 const Completed = () => {
-    return (
-        <Wrapper>
-            <div />
-            <p className='big'> 가입 완료! </p>
-            <p className='small'> 중앙대 멋쟁이 사자처럼 회원이 된 것을 환영합니다! </p>
-        </Wrapper>
-    );
+  return (
+    <Wrapper>
+      <ImageWrapper>
+        <Image src={sign} layout="fill" alt="대체" />
+      </ImageWrapper>
+      <p className="big"> 가입 완료! </p>
+      <p className="small"> 중앙대 멋쟁이 사자처럼 회원이 된 것을 환영합니다! </p>
+    </Wrapper>
+  );
 };
 
 export default Completed;
-
+const ImageWrapper = styled.div`
+  position: relative;
+  width: 360px;
+  height: 360px;
+`;
 const Wrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 
-    div{
-        width: 360px;
-        height: 360px;
-        border: black solid 1px;
-    }
+  p {
+    font-family: 'Pretendard';
+    font-style: normal;
+    margin: 0;
+  }
 
-    p{
-        font-family: 'Pretendard';
-        font-style: normal;
-        margin: 0;
-    }
+  .big {
+    font-size: 4rem;
+    font-weight: 900;
+    margin-top: 7rem;
+  }
 
-    .big{
-        font-size: 4rem;
-        font-weight: 900;
-        margin-top: 7rem;
-    }
-
-    .small{
-        font-size: 1.4rem;
-        font-weight: 500;
-        margin-top: 1rem;
-
-    }
-`
+  .small {
+    font-size: 1.4rem;
+    font-weight: 500;
+    margin-top: 1rem;
+  }
+`;
