5a6db268303268db1865dce4c48ea49cdb199d4a
yunseonyeong <dbstjsdud99@gmail.com>
2023-03-04 21:02:49 +0900

    [CHORE] 아카이빙 API 분리

---
+++ src/apis/mypage.ts
@@ -0,0 +1,59 @@
+import { UserAttendance } from "@@types/request";
+import { IToken } from "@utils/state";
+import axios from "axios";
+import { getAuthAxios } from "./authAxios";
+
+export const getUserAttendance = async (token: IToken) => {
+    const authAxios = getAuthAxios(token);
+    const response = await authAxios.get(`/api/mypage/attendance`);
+    return response.data as UserAttendance;
+    // return {
+    //   name: '윤선영',
+    //   track: 2,
+    //   absence: 3,
+    //   truancy: 1,
+    //   tardiness: 2,
+    // };
+};
+
+export const getTotalAttendance = async (token: IToken) => {
+    const authAxios = getAuthAxios(token);
+    const response = await authAxios.get(`/api/mypage/attendance`);
+    console.log(response.data);
+    return response.data as UserAttendance[];
+
+    // return [{
+    //   name: '윤선영',
+    //   track: 2,
+    //   absence: 3,
+    //   truancy: 1,
+    //   tardiness: 2,
+    // }, {
+    //   name: '김솔',
+    //   track: 0,
+    //   absence: 3,
+    //   truancy: 1,
+    //   tardiness: 2,
+    // }];
+};
+
+export const getAssignments = () => {
+    const data = axios.get(
+        `https://notion-api.splitbee.io/v1/table/${process.env.NEXT_PUBLIC_NOTION_DATABASE_ID}`
+    ).then(res => res.data);
+    return data;
+};
+
+export function editUserScore(userScore: UserAttendance, token: IToken) {
+    const authAxios = getAuthAxios(token);
+    return authAxios.post(
+        `/api/mypage/attendance/`,
+        {
+            name: userScore.name,
+            track: userScore.track,
+            truancy: userScore.truancy,
+            absence: userScore.absence,
+            tardiness: userScore.tardiness,
+        }
+    );
+}
--- src/apis/project.ts
+++ src/apis/project.ts
@@ -1,6 +1,8 @@
-import { ArchivingArrayType, IProjectData } from '@@types/request';
+import { ArchivingArrayType, IProjectData, IProjectDetail } from '@@types/request';
 import axios from 'axios';
 import projectBackupData from './backup/project.json';
+import projectDetailBackupData from './backup/projectDetail.json';
+
 export async function getProjects() {
   try {
     const res = await axios.get<ArchivingArrayType<IProjectData>>(
@@ -9,5 +11,17 @@ export async function getProjects() {
     return res.data;
   } catch (err) {
     return new Promise<ArchivingArrayType<IProjectData>>((resolve) => resolve(projectBackupData));
+  }
+}
+
+export async function getProjectDetail(id: string) {
+  try {
+    const res = await axios.get<IProjectDetail>(
+      `https://286eb829-af4d-43ed-b788-0e8e70ae0820.mock.pstmn.io/project/${id}`,
+      //내맘대로 적은 url임 이런 api없음 바꿔줘야댐
+    );
+    return res.data;
+  } catch (err) {
+    return new Promise<IProjectDetail>((resolve) => resolve(projectDetailBackupData as any));
   }
 }
