4a75c01d02112fb9886bd0bc96e2a15992c657b7
yunseonyeong <dbstjsdud99@gmail.com>
2023-03-06 18:57:52 +0900

    [CHORE] 아카이빙 관련 API 모두 연결 완료

--- src/apis/project.ts
+++ src/apis/project.ts
@@ -1,27 +1,17 @@
-import { ArchivingArrayType, IProjectData, IProjectDetail } from '@@types/request';
+import { ArchivingArrayType, IProjectData, IProjectDetail, ResponseData } from '@@types/request';
 import axios from 'axios';
-import projectBackupData from './backup/project.json';
-import projectDetailBackupData from './backup/projectDetail.json';
 
 export async function getProjects() {
-  try {
-    const res = await axios.get<ArchivingArrayType<IProjectData>>(
-      `https://286eb829-af4d-43ed-b788-0e8e70ae0820.mock.pstmn.io/projects`,
-    );
-    return res.data;
-  } catch (err) {
-    return new Promise<ArchivingArrayType<IProjectData>>((resolve) => resolve(projectBackupData));
-  }
+  const data = await axios.get<ResponseData<ArchivingArrayType<IProjectData>>>(
+    'https://api.cau-likelion.org/project',
+  ).then(res => res.data.data);
+  return data;
 }
 
 export async function getProjectDetail(id: string) {
-  try {
-    const res = await axios.get<IProjectDetail>(
-      `https://286eb829-af4d-43ed-b788-0e8e70ae0820.mock.pstmn.io/project/${id}`,
-      //내맘대로 적은 url임 이런 api없음 바꿔줘야댐
-    );
-    return res.data;
-  } catch (err) {
-    return new Promise<IProjectDetail>((resolve) => resolve(projectDetailBackupData as any));
-  }
+  const data = await axios.get<ResponseData<IProjectDetail>>(
+    `https://api.cau-likelion.org/project/${id}`,
+
+  ).then(res => res.data.data);
+  return data;
 }
