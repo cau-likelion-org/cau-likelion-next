473676ad750c72fe53752d7a2c12630e7dc7d01c
d0422 <rlfehd2013@naver.com>
2023-03-07 03:07:54 +0900

    [FIX] api주소 변경

--- src/apis/project.ts
+++ src/apis/project.ts
@@ -2,16 +2,15 @@ import { ArchivingArrayType, IProjectData, IProjectDetail, ResponseData } from '
 import axios from 'axios';
 
 export async function getProjects() {
-  const data = await axios.get<ResponseData<ArchivingArrayType<IProjectData>>>(
-    'https://api.cau-likelion.org/project',
-  ).then(res => res.data.data);
+  const data = await axios
+    .get<ResponseData<ArchivingArrayType<IProjectData>>>('https://api.cau-likelion.org/api/project')
+    .then((res) => res.data.data);
   return data;
 }
 
 export async function getProjectDetail(id: string) {
-  const data = await axios.get<ResponseData<IProjectDetail>>(
-    `https://api.cau-likelion.org/project/${id}`,
-
-  ).then(res => res.data.data);
+  const data = await axios
+    .get<ResponseData<IProjectDetail>>(`https://api.cau-likelion.org/project/api/${id}`)
+    .then((res) => res.data.data);
   return data;
 }
