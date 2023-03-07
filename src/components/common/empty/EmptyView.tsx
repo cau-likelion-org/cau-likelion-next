d2f40fec9b380662b8f5ab49f345ead25a08d308
yunseonyeong <dbstjsdud99@gmail.com>
2023-03-07 17:09:39 +0900

    [FEAT] 리스트의 길이가 0일때, EmptyComponent 렌더링

---
+++ src/components/common/empty/EmptyView.tsx
@@ -0,0 +1,33 @@
+import { GreyScale } from '@utils/constant/color';
+import React from 'react';
+import styled from 'styled-components';
+
+const EmptyView = () => {
+    return (
+        <Wrapper>
+            <Text>송하야 세션해줘,,</Text>
+        </Wrapper>
+    );
+};
+
+export default EmptyView;
+
+const Wrapper = styled.div`
+    width: 93%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: ${GreyScale.light};
+    height: 330px;
+    border-radius: 20px;
+    margin: 1rem 0 2rem 0;
+`;
+
+const Text = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 2rem;
+    color: ${GreyScale.default};
+    font-family: 'GmarketSans';
+`;
