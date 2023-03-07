a368d31fa43bb4525bce2eca48414aaee1adf7da
d0422 <rlfehd2013@naver.com>
2023-03-08 01:35:04 +0900

    [FEAT] expired시 localstorage 갱신

--- src/apis/authAxios.ts
+++ src/apis/authAxios.ts
@@ -1,8 +1,8 @@
+import LocalStorage from '@utils/localStorage';
 import { IToken } from '@utils/state';
 import axios from 'axios';
 import Axios from 'axios';
 import { getNewToken } from './account';
-
 
 export const getAuthAxios = (token: IToken) => {
   const authAxios = Axios.create({
@@ -20,8 +20,9 @@ export const getAuthAxios = (token: IToken) => {
       if (status !== 401) {
         return Promise.reject(error);
       }
-      const { access: newAccessToken } = await getNewToken(token.refresh);
-
+      const { access: newAccessToken, refresh } = await getNewToken(token.refresh);
+      LocalStorage.setItem('access', newAccessToken);
+      LocalStorage.setItem('refresh', refresh);
       config.headers.Authorization = `Bearer ${newAccessToken}`;
       return axios(config);
     },
