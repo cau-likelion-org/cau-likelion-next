fe51377eb0079750967fcdb75c1ef77d5b958702
yunseonyeong <dbstjsdud99@gmail.com>
2023-03-07 17:57:53 +0900

    [CHORE] 마이페이지 API Response타입 변경

--- src/apis/mypage.ts
+++ src/apis/mypage.ts
@@ -2,63 +2,46 @@ import { RequestEditUserScore, UserAttendance } from '@@types/request';
 import { IToken } from '@utils/state';
 import axios from 'axios';
 import { getAuthAxios } from './authAxios';
+import { ResponseData } from '@@types/request';
 
 export const getUserAttendance = async (token: IToken) => {
-  const authAxios = getAuthAxios(token);
-  const response = await authAxios.get(`/api/mypage/attendance`);
-  return response.data as UserAttendance;
-  // return {
-  //   name: '윤선영',
-  //   track: 2,
-  //   absence: 3,
-  //   truancy: 1,
-  //   tardiness: 2,
-  // };
+    const authAxios = getAuthAxios(token);
+    const data = await authAxios.get<ResponseData<UserAttendance>>(`/api/mypage/attendance`).then(
+        res => res.data.data
+    );
+    return data;
 };
 
 export const getTotalAttendance = async (token: IToken) => {
-  const authAxios = getAuthAxios(token);
-  const response = await authAxios.get(`/api/mypage/attendance`);
-  return response.data as UserAttendance[];
-
-  // return [{
-  //     name: '윤선영',
-  //     track: 2,
-  //     absence: 3,
-  //     truancy: 1,
-  //     tardiness: 2,
-  // }, {
-  //     name: '김솔',
-  //     track: 0,
-  //     absence: 3,
-  //     truancy: 1,
-  //     tardiness: 2,
-  // }];
+    const authAxios = getAuthAxios(token);
+    const data = await authAxios.get<ResponseData<UserAttendance[]>>(`/api/mypage/attendance`)
+        .then(res => res.data.data);
+    return data;
 };
 
 export const getAssignments = () => {
-  const data = axios
-    .get(`https://notion-api.splitbee.io/v1/table/${process.env.NEXT_PUBLIC_NOTION_DATABASE_ID}`)
-    .then((res) => res.data);
-  return data;
+    const data = axios
+        .get(`https://notion-api.splitbee.io/v1/table/${process.env.NEXT_PUBLIC_NOTION_DATABASE_ID}`)
+        .then((res) => res.data);
+    return data;
 };
 
 export function editUserScore(userScore: RequestEditUserScore, token: IToken) {
-  const authAxios = getAuthAxios(token);
-  return authAxios.post(`/api/mypage/attendance/`, {
-    user_id: userScore.user_id,
-    truancy: userScore.truancy,
-    absence: userScore.absence,
-    tardiness: userScore.tardiness,
-  });
+    const authAxios = getAuthAxios(token);
+    return authAxios.post(`/api/mypage/attendance/`, {
+        user_id: userScore.user_id,
+        truancy: userScore.truancy,
+        absence: userScore.absence,
+        tardiness: userScore.tardiness,
+    });
 }
 
 export function makeAttendance(date: string, password: string, token: IToken) {
-  const authAxios = getAuthAxios(token);
-  return authAxios
-    .post('/api/attendance/secret', {
-      date,
-      password,
-    })
-    .then((res) => res.data.data);
+    const authAxios = getAuthAxios(token);
+    return authAxios
+        .post('/api/attendance/secret', {
+            date,
+            password,
+        })
+        .then((res) => res.data.data);
 }
