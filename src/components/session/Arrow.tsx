07c416f8acf0c57e12de629be87d6502b11fe4ed
yunseonyeong <dbstjsdud99@gmail.com>
2023-03-19 18:22:34 +0900

    [REFACTOR] 세션 슬라이더 Arrow 버튼 수정

--- src/components/session/Arrow.tsx
+++ src/components/session/Arrow.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
-import Image from 'next/image';
-import arrow from '@image/Vector 16.png';
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md';
 import { GreyScale } from '@utils/constant/color';
 
-interface ISessionComponent {
-    direction: string;
-    length: number;
-}
-
-const Arrow = ({ direction, length }: { direction: string, length: number; }) => {
+const Arrow = ({ direction, length, onClick }: { direction: string, length: number; onClick: () => void; }) => {
     return (
-        <StArrowWrapper length={length}>
+        <StArrowWrapper length={length} onClick={onClick}>
             {direction === 'left' ? <MdKeyboardArrowLeft size={30} color={GreyScale.default} /> :
                 <MdKeyboardArrowRight size={30} color={GreyScale.default} />}
         </StArrowWrapper>
@@ -24,7 +17,7 @@ export default Arrow;
 
 
 const StArrowWrapper = styled.div<{ length: number; }>`
-visibility: ${props => props.length === 0 ? 'hidden' : 'visible'};
-
-padding-left: 2rem;
+    visibility: ${props => props.length === 0 ? 'hidden' : 'visible'};
+    z-index: 20;
+    cursor: pointer;
 `;
