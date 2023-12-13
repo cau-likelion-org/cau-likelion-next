781de9ee6a09afb8679279b37c122e7b6cf93082
CJY <cjy3458@naver.com>
2023-12-13 22:59:35 +0900

    .

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,5 @@
 import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
-import Script from 'next/script';
-import GA from 'src/test/GA';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -45,7 +43,6 @@ export default class MyDocument extends Document {
           <script src="https://apis.google.com/js/platform.js?onload=init" async defer></script>
         </Head>
         <body>
-          {/* {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS ? <GA ga_id={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} /> : null} */}
           <Main />
           <NextScript />
         </body>
