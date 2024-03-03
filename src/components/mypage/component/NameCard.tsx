5b6dcfff375bc0a0336fe3e53665163aeaed204f
CJY <cjy3458@naver.com>
2024-03-03 21:25:07 +0900

    Feat: 로그아웃 버튼 제거

--- src/components/mypage/component/NameCard.tsx
+++ src/components/mypage/component/NameCard.tsx
@@ -3,42 +3,42 @@ import React from 'react';
 import styled from 'styled-components';
 import LogoutButton from './LogoutButton';
 
-const NameCard = ({ name, generation }: { name: string; generation: number; }) => {
-    return (
-        <Wrapper>
-            <Text>
-                <span>{generation}</span>기 &nbsp;
-                <span>{name}</span>님
-            </Text>
-            <LogoutButton />
-        </Wrapper>
-    );
+const NameCard = ({ name, generation }: { name: string; generation: number }) => {
+  return (
+    <Wrapper>
+      <Text>
+        <span>{generation}</span>기 &nbsp;
+        <span>{name}</span>님
+      </Text>
+      {/* <LogoutButton /> */}
+    </Wrapper>
+  );
 };
 
 export default NameCard;
 
 const Wrapper = styled.div`
-    display: flex;
-    margin: 1rem 0;
-    width: 40%;
-    justify-content: flex-start;
-    gap: 10px;
-    @media (max-width: 900px) {
-        width: 80%;
-    }
+  display: flex;
+  margin: 1rem 0;
+  width: 40%;
+  justify-content: flex-start;
+  gap: 10px;
+  @media (max-width: 900px) {
+    width: 80%;
+  }
 `;
 
 const Text = styled.div`
-    font-family: 'Pretendard';
-    font-style: normal;
-    font-weight: 500;
-    font-size: 2.5rem;
-    color: ${Basic.default};
-    line-height: 3rem;
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 2.5rem;
+  color: ${Basic.default};
+  line-height: 3rem;
 
-    > span{
-        font-weight: 900;
-        font-size: 3rem;
-        text-align: center;
-    }
+  > span {
+    font-weight: 900;
+    font-size: 3rem;
+    text-align: center;
+  }
 `;
