e81888178cbeb6753e1045d83117293d8616d196
suzzang2 <subin030102@naver.com>
2025-03-21 12:50:47 +0900

    FEAT: 아카이빙-점검중 해제

--- src/pages/gallery/index.tsx
+++ src/pages/gallery/index.tsx
@@ -12,10 +12,9 @@ import styled from 'styled-components';
 const GalleryList = ({ galleryStaticData }: { galleryStaticData: ArchivingArrayType<IGalleryData> }) => {
   return (
     <>
-      <div>점검중입니다.</div>
-      {/* <ListPageHead category={ARCHIVING.GALLERY} canoUrl={'https://cau-likelion.org/gallery'} />
+      <ListPageHead category={ARCHIVING.GALLERY} canoUrl={'https://cau-likelion.org/gallery'} />
       <Header pageName="추억" introduce="멋사와 함께 한 추억" />
-      <GalleryListSection staticData={galleryStaticData} /> */}
+      <GalleryListSection staticData={galleryStaticData} />
     </>
   );
 };
