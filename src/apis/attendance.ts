5efae1a4d9ab0ec86ea8b98ac5b20b0e6cf67d71
d0422 <rlfehd2013@naver.com>
2025-09-21 12:54:06 +0900

    chore: 불필요 코드제거

--- src/apis/attendance.ts
+++ src/apis/attendance.ts
@@ -1,33 +1,9 @@
-import { TodayAttendanceData, TodayAttendanceListData } from '@@types/request';
 import { IToken } from '@utils/state';
 import { getAuthAxios } from './authAxios';
-import { toDateString } from '@utils/index';
 
-export function getAttendance(token: IToken) {
-  const authAxios = getAuthAxios(token);
-  return authAxios
-    .get(`/api/attendance`, {
-      params: {
-        date: toDateString(),
-      },
-    })
-    .then((res) => {
-      return res.data.data as TodayAttendanceData;
-    });
-}
 export function postAttendance(password: string, token: IToken) {
   const authAxios = getAuthAxios(token);
   return authAxios.post(`/api/attendance`, {
     password: password,
   });
-}
-export function getAttendanceList(token: IToken) {
-  const authAxios = getAuthAxios(token);
-  return authAxios
-    .get(`/api/attendance/list`, {
-      params: {
-        date: toDateString(),
-      },
-    })
-    .then((res) => res.data.data as TodayAttendanceListData);
 }
