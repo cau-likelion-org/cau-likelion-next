d326afdad54b897343ea2624245afe49d8ce39ad
Jihaeee <elinkang02@gmail.com>
2026-07-27 19:15:24 +0900

    chore: 추상화 적용

---
+++ src/assets/svg/index.ts
@@ -0,0 +1,5 @@
+import IcCircleCheck from './ic-circle-check.svg';
+import IcCircleExclamation from './ic-circle-exclamation.svg';
+import IcTriangleExclamation from './ic-triangle-exclamation.svg';
+
+export { IcCircleCheck, IcCircleExclamation, IcTriangleExclamation };
