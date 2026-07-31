2dc52163684277c9517297bb0f4fe99818a3c269
eunjilee0311 <leg040311@gmail.com>
2026-08-01 04:00:22 +0900

    feat: 활동 소개 섹션을 카드 리스트로 개편
    - 원형 갤러리 구성을 세션/프로젝트/선배와의 만남/스터디/
      소모임 카드 리스트로 교체
    - 배경을 화면 폭 전체로 확장 (width:1440px, 조상 flex
      가운데 정렬에 의존)
    - 더 이상 쓰이지 않는 CircleComponent 제거

--- src/components/home/activity/ActivitySection.tsx
+++ src/components/home/activity/ActivitySection.tsx
@@ -1,124 +1,141 @@
-import React from 'react';
 import styled from 'styled-components';
-import QuestionLion from '@image/가치_물음표 사자.gif';
-import Image, { StaticImageData } from 'next/image';
-import FadeInComponent from '@home/common/FadeInComponent';
-import CircleComponent from './component/CircleComponent';
-import 해커톤 from '@image/해커톤.png';
-import 아이디어톤 from '@image/아이디어톤.png';
-import 스터디 from '@image/스터디.png';
-import 정기세션 from '@image/정기세션.png';
-import 중커톤 from '@image/중커톤.png';
 
-export interface IActivityData {
+import { Black, BackgroundWhite, BackgroundLight, Line, Fill } from '@utils/constant/color';
+import { Typography, typographyCss } from '@utils/constant/typography';
+
+interface IActivity {
   title: string;
-  text: string;
-  image: StaticImageData;
+  subtitle: string;
+  description: string;
 }
+
+const DESCRIPTION =
+  '소개글 소개글 소개글 소개글 소개글 소개글 소개글 소개글 소개글 소개글 소개글 소개글 소개글 소개글 소개글 소개글 소개글 소개글 소개글 소개글 소개글 소개글 소개글 소개글 소개글 소개글 소개글 소개글 소개글 소개글 소개글 소개글 소개글 소개글 소개글 소개글 소개글 소개글 소개글 소개글';
+const SUBTITLE = '일주일에 1번 정기적 대면 모임';
+
+const ACTIVITIES: IActivity[] = [
+  { title: '세션', subtitle: SUBTITLE, description: DESCRIPTION },
+  { title: '프로젝트', subtitle: SUBTITLE, description: DESCRIPTION },
+  { title: '선배와의 만남', subtitle: SUBTITLE, description: DESCRIPTION },
+  { title: '스터디', subtitle: SUBTITLE, description: DESCRIPTION },
+  { title: '소모임', subtitle: SUBTITLE, description: DESCRIPTION },
+];
+
 const ActivitySection = () => {
-  const data = [
-    {
-      title: '정기세션',
-      text: '트랙별 정기 세션',
-      image: 정기세션,
-    },
-    {
-      title: '아이디어톤',
-      text: '해커톤을 위한 청사진',
-      image: 아이디어톤,
-    },
-    {
-      title: '해커톤',
-      text: '아이디어 실현의 장',
-      image: 해커톤,
-    },
-    {
-      title: '중커톤',
-      text: '중앙대 멋사만의 해커톤',
-      image: 중커톤,
-    },
-    {
-      title: '스터디',
-      text: '함께 공부하며 성장하기',
-      image: 스터디,
-    },
-  ] as IActivityData[];
   return (
-    <FadeInComponent>
-      <Wrapper>
-        <ImageWrapper>
-          <Image src={QuestionLion} fill style={{ objectFit: 'fill', objectPosition: 'center' }} alt="소개하는 사자" />
-        </ImageWrapper>
-        <TitleText>멋사의 다양한 활동</TitleText>
-        <ThreeCircleWrapper>
-          {Array.from({ length: 3 }).map((_, index) => (
-            <CircleComponent key={index} data={data[index]} />
-          ))}
-        </ThreeCircleWrapper>
-        <TwoCircleWrapper>
-          {Array.from({ length: 2 }).map((_, index) => (
-            <CircleComponent key={index + 3} data={data[index + 3]} />
-          ))}
-        </TwoCircleWrapper>
-      </Wrapper>
-    </FadeInComponent>
+    <Wrapper>
+      <Content>
+        <Title>활동 소개</Title>
+        <ListGroup>
+          <List>
+            {ACTIVITIES.map(({ title, subtitle, description }) => (
+              <Card key={title}>
+                <TextGroup>
+                  <CardTitle>{title}</CardTitle>
+                  <CardSubtitle>{subtitle}</CardSubtitle>
+                  <CardDescription>{description}</CardDescription>
+                </TextGroup>
+                <Thumbnail />
+              </Card>
+            ))}
+          </List>
+          <Footnote>*출처정보 (2026년 02월 기준)</Footnote>
+        </ListGroup>
+      </Content>
+    </Wrapper>
   );
 };
 
 export default ActivitySection;
 
 const Wrapper = styled.div`
+  width: 1440px;
+  padding: 80px 0;
+  display: flex;
+  justify-content: center;
+  background-color: ${BackgroundLight.secondary};
+  scroll-snap-align: start;
+`;
+
+const Content = styled.div`
+  width: 1060px;
   display: flex;
   flex-direction: column;
   align-items: center;
+  gap: 52px;
+`;
+
+const Title = styled.p`
+  ${typographyCss(Typography.display2.bold)}
+  color: ${Black.b900};
+  margin: 0;
+`;
+
+const ListGroup = styled.div`
   width: 100%;
-  @media (min-width: 1850px) {
-    margin-top: 20rem;
-  }
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 28px;
 `;
 
-const ThreeCircleWrapper = styled.div`
+const List = styled.div`
+  width: 100%;
   display: flex;
-  justify-content: space-evenly;
-  gap: 50px;
-  @media (max-width: 400px) {
-    gap: 20px;
-  }
-`;
-const TwoCircleWrapper = styled.div`
-  display: flex;
-  gap: 50px;
-  align-items: center;
-  justify-content: center;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 20px;
 `;
 
-const ImageWrapper = styled.div`
-  position: relative;
-  width: 150px;
-  height: 150px;
-  @media (max-width: 900px) {
-    width: 80px;
-    height: 80px;
-  }
-  @media (max-width: 1200px) {
-    width: 130px;
-    height: 130px;
-  }
-  filter: brightness(50%);
-  border-radius: 50%;
+const Card = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: flex-start;
+  gap: 14px;
+  padding: 32px;
+  border-radius: 22px;
+  background-color: ${BackgroundWhite.secondary};
+  box-shadow: inset 0 0 0 1px ${Line.subtle};
 `;
 
-const TitleText = styled.div`
+const TextGroup = styled.div`
+  flex: 1 0 0;
+  min-width: 0;
   display: flex;
-  justify-content: flex-start;
-  font-family: 'Gmarket Sans';
-  font-style: normal;
-  font-weight: 800;
-  line-height: 4rem;
-  margin: 2.5rem 0;
-  font-size: 3rem;
-  @media (max-width: 900px) {
-    justify-content: center;
-    font-size: 2.5rem;
-  }
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 14px;
+`;
+
+const CardTitle = styled.p`
+  ${typographyCss(Typography.title2.bold)}
+  color: ${Black.b900};
+  margin: 0;
+`;
+
+const CardSubtitle = styled.p`
+  ${typographyCss(Typography.headline1.regular)}
+  color: ${Black.b80};
+  margin: 0;
+`;
+
+const CardDescription = styled.p`
+  ${typographyCss(Typography.body1Normal.medium)}
+  color: ${Black.b900};
+  margin: 0;
+`;
+
+const Thumbnail = styled.div`
+  flex-shrink: 0;
+  width: 300px;
+  height: 169px;
+  border-radius: 12px;
+  background-color: ${Fill.normal};
+`;
+
+const Footnote = styled.p`
+  ${typographyCss(Typography.caption1.regular)}
+  color: ${Black.b50};
+  align-self: flex-start;
+  margin: 0;
 `;
