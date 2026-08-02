e17e5a3d03e2b951018d1964661a19c646eaa0bd
eunjilee0311 <leg040311@gmail.com>
2026-08-03 01:12:43 +0900

    style: pages/blog/index.tsx의 ReactElement import를 type-only로 변경

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from 'react';
+import type { ReactElement } from 'react';
 import LayoutArchiving from '@common/layout/LayoutArchiving';
 import BlogListSection from '@blog/BlogListSection';
 
